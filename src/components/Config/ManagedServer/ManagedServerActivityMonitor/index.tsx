5411d973ba21cc9fc4a6d1a47d359729b1ead20d
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-12-25 11:47:10 -0500

    Add page visibility tracking and update activity for managed server
    This commit adds functionality to track the visibility of the page and update the activity for the managed server accordingly. It also sets up an event listener to handle page visibility changes and updates the activity every 60 seconds. Additionally, it handles the auto creation of a new managed server if the previous one was auto destroyed.

--- src/components/Config/ManagedServer/ManagedServerActivityMonitor/index.tsx
+++ src/components/Config/ManagedServer/ManagedServerActivityMonitor/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useQueryClient } from "react-query";
 import { useDeployManagedServer } from "../../../../hooks/useDeployManagedServer";
 import useInterval from "../../../../hooks/useInterval";
@@ -6,6 +6,7 @@ import { useManagedServer, useManagedServerActivityUpdate } from "../../../../ho
 
 export default function ManagedServerActivityMonitor() {
   const [isPageVisible, setPageVisible] = useState(!document.hidden);
+  const isPageVisibleRef = useRef(isPageVisible);
 
   const { data: managedServer } = useManagedServer();
   const { mutate: updateActivity } = useManagedServerActivityUpdate();
@@ -13,12 +14,26 @@ export default function ManagedServerActivityMonitor() {
   const { handleDeploy } = useDeployManagedServer();
   const queryClient = useQueryClient();
 
+  /**
+   * runs when the page visibility changes.
+   * It invalidates the "get-managed-server" query in the query client
+   * and updates the activity for the managed server if it is defined.
+   *
+   */
   useEffect(() => {
     if (isPageVisible) {
       queryClient.invalidateQueries("get-managed-server");
+      if (managedServer !== undefined) {
+        updateActivity(managedServer.userPrincipalName);
+      }
     }
+
+    isPageVisibleRef.current = isPageVisible;
   }, [isPageVisible]);
 
+  /**
+   * handles auto creation of a new managed server if the previous one was auto destroyed.
+   */
   useEffect(() => {
     if (managedServer === undefined) {
       return;
@@ -26,23 +41,32 @@ export default function ManagedServerActivityMonitor() {
 
     // if managed server was auto destroyed and the page is visible, auto create a new one
     if (managedServer.status === "AutoDestroyed" && isPageVisible && managedServer.autoCreate) {
+      // wait for 10 seconds before creating a new managed server
       setTimeout(() => {
-        if (isPageVisible) {
+        if (isPageVisibleRef.current) {
+          console.log("Auto creating a new managed server.");
           handleDeploy(managedServer);
         }
       }, 10000); // 10 seconds delay
     }
   }, [managedServer]);
 
+  /**
+   * Sets up an event listener to track the visibility of the page and update the state accordingly.
+   */
   useEffect(() => {
     function handleVisibilityChange() {
       setPageVisible(!document.hidden);
+      console.log("Page visibility changed to: ", !document.hidden);
     }
 
     document.addEventListener("visibilitychange", handleVisibilityChange);
     return () => document.removeEventListener("visibilitychange", handleVisibilityChange);
   }, []);
 
+  /**
+   * Updates the activity for the managed server every 60 seconds.
+   */
   useInterval(() => {
     if (!isPageVisible || managedServer === undefined) {
       return;
