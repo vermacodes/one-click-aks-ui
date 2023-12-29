5695478f706375fd04da4de50b9ab538a5e1e5fa
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-12-29 14:40:45 -0500

    Add server hosting configuration and update server not connected component

--- src/components/Detectors/ServerNotConnected/index.tsx
+++ src/components/Detectors/ServerNotConnected/index.tsx
@@ -1,29 +1,70 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { ServerHosting } from "../../../dataStructures";
+import { useManagedServer } from "../../../hooks/useManagedServer";
 import { useServerStatus } from "../../../hooks/useServerStatus";
 
 export default function ServerNotConnected() {
+  const [severHosting, setServerHosting] = useState<ServerHosting>({
+    environment: "docker",
+    endpoint: "http://localhost:8880/",
+  });
   const { data: serverStatus, isError } = useServerStatus();
+  const { data: managedServer } = useManagedServer();
+
+  useEffect(() => {
+    const serverHostingFromLocalStorage = localStorage.getItem("serverHosting");
+    if (serverHostingFromLocalStorage != undefined && serverHostingFromLocalStorage !== "") {
+      setServerHosting(JSON.parse(serverHostingFromLocalStorage));
+    }
+  }, []);
 
   if (!isError && serverStatus && serverStatus.status === "OK") {
     return <></>;
   }
 
+  if (severHosting.environment === "docker") {
+    return (
+      <div className="my-4">
+        <div className="mt-2 rounded border border-red-500 bg-red-500 bg-opacity-20 p-2">
+          <strong>🛑 Server Not Connected:</strong> Your self-hosted server is not available. Check your{" "}
+          <Link to="/settings" className="cursor-pointer text-sky-600 underline">
+            Settings
+          </Link>{" "}
+          to make sure server is deployed and <a className="underline">endpoint</a> is correct.
+        </div>
+      </div>
+    );
+  }
+
+  if (managedServer?.status === "Deploying") {
+    return <></>;
+  }
+
+  if (managedServer?.status === "Destroyed") {
+    return (
+      <div className="my-4">
+        <div className="mt-2 rounded border border-red-500 bg-red-500 bg-opacity-20 p-2">
+          <strong>🛑 Server Not Deployed:</strong> You are using managed server but its not deployed. Please deploy your
+          managed server from{" "}
+          <Link to="/settings" className="cursor-pointer text-sky-600 underline">
+            Settings
+          </Link>
+          .
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="my-4">
       <div className="mt-2 rounded border border-red-500 bg-red-500 bg-opacity-20 p-2">
-        <strong>🛑 Server Not Connected:</strong> ACT Labs requires <a className="underline">user to host their own server</a>.{" "}
-        <a
-          href="https://dev.azure.com/Supportability/AzureContainers/_wiki/wikis/Containers%20Wiki/849282/Getting-Started"
-          target="_blank"
-          className="cursor-pointer text-sky-600 underline"
-        >
-          Follow these instructions to host your own server
-        </a>{" "}
-        and check your{" "}
+        <strong>🛑 Server Not Connected:</strong> Not able to connect to <a className="underline">managed server</a>.{" "}
+        Try refreshing page or re-deploying server from{" "}
         <Link to="/settings" className="cursor-pointer text-sky-600 underline">
           Settings
-        </Link>{" "}
-        to make sure the <a className="underline">Server Endpoint</a> is correct.{" "}
+        </Link>
+        .
       </div>
     </div>
   );
