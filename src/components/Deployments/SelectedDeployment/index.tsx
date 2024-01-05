84b6a93156857e8ad56ceea8d95520ed33d8a98b
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-05 13:10:41 -0500

    Add new lab button and fix container styling

--- src/components/Deployments/SelectedDeployment/index.tsx
+++ src/components/Deployments/SelectedDeployment/index.tsx
@@ -1,17 +1,17 @@
-import Button from "../../UserInterfaceComponents/Button";
-import { FaPlus, FaBinoculars } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { FaBinoculars } from "react-icons/fa";
 import { SiTerraform } from "react-icons/si";
-import { useGetMyDeployments } from "../../../hooks/useDeployments";
 import { Link } from "react-router-dom";
+import { DeploymentType } from "../../../dataStructures";
+import { useGetMyDeployments } from "../../../hooks/useDeployments";
+import { useSelectedDeployment } from "../../../hooks/useSelectedDeployment";
+import Button from "../../UserInterfaceComponents/Button";
+import Container from "../../UserInterfaceComponents/Container";
 import AutoDestroySwitch from "../AutoDestroySwitch";
-import DestroyTime from "../DestroyTime";
+import BreakBlobLease from "../BreakBlobLease";
 import DeploymentLifespan from "../DeploymentLifespan";
 import DeploymentStatus from "../DeploymentStatus";
-import CreateNewDeployment from "../CreateNewDeployment";
-import BreakBlobLease from "../BreakBlobLease";
-import { useSelectedDeployment } from "../../../hooks/useSelectedDeployment";
-import { DeploymentType } from "../../../dataStructures";
-import { useEffect, useState } from "react";
+import DestroyTime from "../DestroyTime";
 
 type Props = {
   sticky?: boolean;
@@ -20,19 +20,13 @@ type Props = {
 export default function SelectedDeployment({ sticky = true }: Props) {
   const { data: deployments } = useGetMyDeployments();
   const { selectedDeployment } = useSelectedDeployment();
-  const [selectedDeploymentState, setSelectedDeploymentState] = useState<
-    DeploymentType | undefined
-  >(undefined);
+  const [selectedDeploymentState, setSelectedDeploymentState] = useState<DeploymentType | undefined>(undefined);
 
   useEffect(() => {
     if (selectedDeployment !== undefined) {
       setSelectedDeploymentState(selectedDeployment);
     } else if (deployments !== undefined) {
-      setSelectedDeploymentState(
-        deployments.filter(
-          (deployment) => deployment.deploymentWorkspace === "default"
-        )[0]
-      );
+      setSelectedDeploymentState(deployments.filter((deployment) => deployment.deploymentWorkspace === "default")[0]);
     }
   }, [selectedDeployment, deployments]);
 
@@ -41,20 +35,11 @@ export default function SelectedDeployment({ sticky = true }: Props) {
   }
 
   return (
-    <div
-      className={`${
-        sticky && "sticky top-0 z-20"
-      } mb-3 rounded bg-slate-50 p-3 shadow outline-1 outline-sky-500 hover:outline dark:bg-slate-900`}
-    >
-      <div
-        className="flex justify-between gap-2 text-sm"
-        key={selectedDeploymentState.deploymentId}
-      >
+    <Container sticky={sticky} additionalClasses="mb-4 p-2 text-base">
+      <div className="flex justify-between gap-2 text-sm" key={selectedDeploymentState.deploymentId}>
         <div className="flex items-center gap-x-2 ">
           <SiTerraform className="text-xl" />
-          <h1 className="text-xl text-sky-500">
-            {selectedDeploymentState.deploymentWorkspace}
-          </h1>
+          <h1 className="text-xl text-sky-500">{selectedDeploymentState.deploymentWorkspace}</h1>
         </div>
         <div className="flex flex-wrap items-center justify-end gap-2">
           <DeploymentStatus deployment={selectedDeploymentState} />
@@ -68,28 +53,17 @@ export default function SelectedDeployment({ sticky = true }: Props) {
           <DeploymentLifespan deployment={selectedDeploymentState} />
           <DestroyTime deployment={selectedDeploymentState} />
           <div className="mx-2 h-6 border-r border-gray-300"></div>
-          <BreakBlobLease
-            deployment={selectedDeploymentState}
-            buttonVariant="secondary-text"
-          />
+          <BreakBlobLease deployment={selectedDeploymentState} buttonVariant="secondary-text" />
           <Link to={"/deployments"}>
-            <Button
-              variant="secondary-text"
-              tooltipMessage="View all deployments"
-              tooltipDelay={200}
-            >
-              <FaBinoculars /> View
+            <Button variant="secondary-text" tooltipMessage="View all deployments" tooltipDelay={200}>
+              <FaBinoculars /> View All
             </Button>
           </Link>
-          <CreateNewDeployment
-            variant="primary-text"
-            tooltipMessage="Add new deployment"
-            tooltipDelay={200}
-          >
+          {/* <CreateNewDeployment variant="primary-text" tooltipMessage="Add new deployment" tooltipDelay={200}>
             <FaPlus /> Add
-          </CreateNewDeployment>
+          </CreateNewDeployment> */}
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
