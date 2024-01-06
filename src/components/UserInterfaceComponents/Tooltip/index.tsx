a8a0b74e8e07f071e82550d187b963f42d51d589
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-06 14:44:02 -0500

    Refactor Tooltip component to include align prop

--- src/components/UserInterfaceComponents/Tooltip/index.tsx
+++ src/components/UserInterfaceComponents/Tooltip/index.tsx
@@ -5,16 +5,13 @@ type Props = {
   children: React.ReactNode;
   delay?: number; // in milliseconds
   direction?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
 };
 
-export default function Tooltip({
-  message,
-  children,
-  delay = 100,
-  direction = "bottom",
-}: Props) {
+export default function Tooltip({ message, children, delay = 100, direction = "bottom", align = "center" }: Props) {
   const [visible, setVisible] = useState(false);
   const [mouseOn, setMouseOn] = useState(false);
+
   let timer: NodeJS.Timeout;
 
   useEffect(() => {
@@ -29,21 +26,19 @@ export default function Tooltip({
   }, [mouseOn]);
 
   return (
-    <div
-      className="relative flex"
-      onMouseEnter={() => setMouseOn(true)}
-      onMouseLeave={() => setMouseOn(false)}
-    >
+    <div className="relative flex" onMouseEnter={() => setMouseOn(true)} onMouseLeave={() => setMouseOn(false)}>
       {children}
       {message && visible && (
         <div
-          className={`
-          absolute z-10 transform rounded bg-slate-800 p-2 text-xs text-slate-100 transition-all
-          dark:bg-slate-100 dark:text-slate-900 
-          ${direction === "top" ? "bottom-12" : ""}
-          ${direction === "bottom" ? "top-12" : ""}
-          ${direction === "left" ? "right-12" : ""}
-          ${direction === "right" ? "left-12" : ""}
+          className={`absolute
+          z-10 w-48 min-w-full transform whitespace-pre-wrap rounded bg-slate-800 p-2 text-xs text-slate-100
+          transition-all  dark:bg-slate-100 dark:text-slate-900 
+          ${direction === "top" ? "bottom-full -translate-y-2" : ""}
+          ${direction === "bottom" ? "top-full translate-y-2" : ""}
+          ${direction === "left" ? "right-full -translate-x-2" : ""}
+          ${direction === "right" ? "left-full translate-x-2" : ""}
+          ${align === "center" ? "left-1/2 -translate-x-1/2" : ""}
+          ${align === "end" ? "right-0" : ""}
         `}
         >
           {message}
