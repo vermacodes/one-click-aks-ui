95f57bb95f4366b71bfad462ee6deb3a9a8faf1b
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-07 00:07:02 -0500

    Remove unused code in MyAssignments component

--- src/pages/MyAssignments/index.tsx
+++ src/pages/MyAssignments/index.tsx
@@ -2,14 +2,11 @@ import { useEffect } from "react";
 import CreateMyAssignmentContainer from "../../components/Lab/Assignment/CreateAssignment/CreateMyAssignmentContainer";
 import ListMyAssignment from "../../components/Lab/Assignment/ListMyAssignment";
 import Terminal from "../../components/Terminal";
-import { useGetMyProfile } from "../../hooks/useProfile";
 import PageLayout from "../../layouts/PageLayout";
 
 type Props = {};
 
 export default function MyAssignments({}: Props) {
-  const { data: myProfile } = useGetMyProfile();
-
   useEffect(() => {
     document.title = "ACT Labs | My Assignments";
   }, []);
