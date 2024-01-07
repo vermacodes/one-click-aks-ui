8328240f3f061de636733e60b9ccf6f20a485aa1
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-06 22:54:31 -0500

    Update ResetLabState button variant

--- src/components/UserInterfaceComponents/Button/index.tsx
+++ src/components/UserInterfaceComponents/Button/index.tsx
@@ -8,40 +8,40 @@ type VariantStyles = {
 
 const variantStyles: VariantStyles = {
   primary:
-    "border-1 border-sky-600 bg-sky-600 py-1 px-3 text-white disabled:border-slate-400 disabled:bg-slate-400 hover:border-sky-700 hover:bg-sky-700 disabled:hover:border-slate-400 disabled:hover:bg-slate-400 dark:disabled:border-slate-700 dark:disabled:bg-slate-700 dark:disabled:text-slate-500",
+    "ring-1 ring-sky-600 bg-sky-600 py-1 px-3 text-white disabled:ring-slate-400 disabled:bg-slate-400 hover:ring-sky-700 hover:bg-sky-700 disabled:hover:ring-slate-400 disabled:hover:bg-slate-400 dark:disabled:ring-slate-700 dark:disabled:bg-slate-700 dark:disabled:text-slate-500",
   secondary:
-    "border-1 border-slate-500 bg-slate-500 py-1 px-3 text-white disabled:border-slate-400 disabled:bg-slate-400 hover:border-slate-700 hover:bg-slate-700 disabled:hover:border-slate-400 disabled:hover:bg-slate-400 dark:disabled:border-slate-700 dark:disabled:bg-slate-700 dark:disabled:text-slate-500",
+    "ring-1 ring-slate-500 bg-slate-500 py-1 px-3 text-white disabled:ring-slate-400 disabled:bg-slate-400 hover:ring-slate-700 hover:bg-slate-700 disabled:hover:ring-slate-400 disabled:hover:bg-slate-400 dark:disabled:ring-slate-700 dark:disabled:bg-slate-700 dark:disabled:text-slate-500",
   danger:
-    "border-1 border-rose-500 bg-rose-500 py-1 px-3 text-white disabled:border-slate-400 disabled:bg-slate-400 hover:border-rose-700 hover:bg-rose-700 disabled:hover:border-slate-400 disabled:hover:bg-slate-400 dark:disabled:border-slate-700 dark:disabled:bg-slate-700 dark:disabled:text-slate-500",
+    "ring-1 ring-rose-500 bg-rose-500 py-1 px-3 text-white disabled:ring-slate-400 disabled:bg-slate-400 hover:ring-rose-700 hover:bg-rose-700 disabled:hover:ring-slate-400 disabled:hover:bg-slate-400 dark:disabled:ring-slate-700 dark:disabled:bg-slate-700 dark:disabled:text-slate-500",
   success:
-    "border-1 border-green-500 bg-green-500 py-1 px-3 text-white disabled:border-slate-400 disabled:bg-slate-400 hover:border-green-700 hover:bg-green-700 disabled:hover:border-slate-400 disabled:hover:bg-slate-400 dark:disabled:border-slate-700 dark:disabled:bg-slate-700 dark:disabled:text-slate-500 ",
+    "ring-1 ring-green-500 bg-green-500 py-1 px-3 text-white disabled:ring-slate-400 disabled:bg-slate-400 hover:ring-green-700 hover:bg-green-700 disabled:hover:ring-slate-400 disabled:hover:bg-slate-400 dark:disabled:ring-slate-700 dark:disabled:bg-slate-700 dark:disabled:text-slate-500 ",
   "primary-outline":
-    "border-1 border-sky-500 py-1 px-3 text-sky-500 disabled:border-slate-400 disabled:text-slate-400 hover:border-sky-700 hover:bg-sky-700  hover:text-slate-100 disabled:hover:border-slate-400 disabled:hover:bg-inherit disabled:hover:text-slate-400 dark:disabled:border-slate-700 dark:disabled:text-slate-500 ",
+    "ring-1 ring-sky-500 py-1 px-3 text-sky-500 disabled:ring-slate-400 disabled:text-slate-400 hover:ring-sky-700 hover:bg-sky-700  hover:text-slate-100 disabled:hover:ring-slate-400 disabled:hover:bg-inherit disabled:hover:text-slate-400 dark:disabled:ring-slate-700 dark:disabled:text-slate-500 ",
   "secondary-outline":
-    "border-1 border-slate-500 py-1 px-3 text-slate-500 disabled:border-slate-400 disabled:text-slate-400 hover:border-slate-500 hover:bg-slate-500  hover:text-slate-100 disabled:hover:border-slate-400 disabled:hover:bg-inherit disabled:hover:text-slate-400 dark:disabled:border-slate-700 dark:disabled:text-slate-500 ",
+    "ring-1 ring-slate-500 py-1 px-3 text-slate-500 disabled:ring-slate-400 disabled:text-slate-400 hover:ring-slate-700 hover:bg-slate-500  hover:text-slate-100 disabled:hover:ring-slate-400 disabled:hover:bg-inherit disabled:hover:text-slate-400 dark:disabled:ring-slate-700 dark:disabled:text-slate-500 ",
   "danger-outline":
-    "border-1 border-rose-500 py-1 px-3 text-rose-500 disabled:border-slate-400 disabled:text-slate-400 hover:border-rose-500 hover:bg-rose-700  hover:text-slate-100 disabled:hover:border-slate-400 disabled:hover:bg-inherit disabled:hover:text-slate-400 dark:disabled:border-slate-700 dark:disabled:text-slate-500 ",
+    "ring-1 ring-rose-500 py-1 px-3 text-rose-500 disabled:ring-slate-400 disabled:text-slate-400 hover:ring-rose-700 hover:bg-rose-700  hover:text-slate-100 disabled:hover:ring-slate-400 disabled:hover:bg-inherit disabled:hover:text-slate-400 dark:disabled:ring-slate-700 dark:disabled:text-slate-500 ",
   "success-outline":
-    "border-1 border-green-500 py-1 px-3 text-green-500 disabled:border-slate-400 disabled:text-slate-400 hover:border-green-500 hover:bg-green-700  hover:text-slate-100 disabled:hover:border-slate-400 disabled:hover:bg-inherit disabled:hover:text-slate-400 dark:disabled:border-slate-700 dark:disabled:text-slate-500 ",
+    "ring-1 ring-green-500 py-1 px-3 text-green-500 disabled:ring-slate-400 disabled:text-slate-400 hover:ring-green-700 hover:bg-green-700  hover:text-slate-100 disabled:hover:ring-slate-400 disabled:hover:bg-inherit disabled:hover:text-slate-400 dark:disabled:ring-slate-700 dark:disabled:text-slate-500 ",
   "primary-text":
-    "border-1 border-slate-50 py-1 px-3 disabled:text-slate-400 hover:border-sky-500 hover:bg-sky-500 hover:bg-opacity-20 disabled:hover:border-slate-50 disabled:hover:bg-slate-50 dark:border-slate-900 dark:hover:border-sky-500 dark:disabled:hover:border-slate-900 dark:disabled:hover:bg-slate-900",
+    "ring-0 hover:ring-1 ring-slate-50 py-1 px-3 disabled:text-slate-400 hover:ring-sky-500 hover:bg-sky-500 hover:bg-opacity-20 disabled:hover:ring-slate-50 disabled:hover:bg-slate-50 dark:ring-slate-900 dark:hover:ring-sky-500 dark:disabled:hover:ring-slate-900 dark:disabled:hover:bg-slate-900",
   "secondary-text":
-    "border-1 border-slate-50 py-1 px-3 disabled:text-slate-400 hover:border-slate-500 hover:bg-slate-500 hover:bg-opacity-20 disabled:hover:border-slate-50 disabled:hover:bg-slate-50 dark:border-slate-900 dark:hover:border-slate-500 dark:disabled:hover:border-slate-900 dark:disabled:hover:bg-slate-900",
+    "ring-0 hover:ring-1 ring-slate-50 py-1 px-3 disabled:text-slate-400 hover:ring-slate-500 hover:bg-slate-500 hover:bg-opacity-20 disabled:hover:ring-slate-50 disabled:hover:bg-slate-50 dark:ring-slate-900 dark:hover:ring-slate-500 dark:disabled:hover:ring-slate-900 dark:disabled:hover:bg-slate-900",
   "danger-text":
-    "border-1 border-slate-50 py-1 px-3 disabled:text-slate-400 hover:border-rose-500 hover:bg-rose-500 hover:bg-opacity-20 disabled:hover:border-slate-50 disabled:hover:bg-slate-50 dark:border-slate-900 dark:hover:border-rose-500 dark:disabled:hover:border-slate-900 dark:disabled:hover:bg-slate-900",
+    "ring-0 hover:ring-1 ring-slate-50 py-1 px-3 disabled:text-slate-400 hover:ring-rose-500 hover:bg-rose-500 hover:bg-opacity-20 disabled:hover:ring-slate-50 disabled:hover:bg-slate-50 dark:ring-slate-900 dark:hover:ring-rose-500 dark:disabled:hover:ring-slate-900 dark:disabled:hover:bg-slate-900",
   "success-text":
-    "border-1 border-slate-50 py-1 px-3 disabled:text-slate-400 hover:border-green-500 hover:bg-green-500 hover:bg-opacity-20 disabled:hover:border-slate-50 disabled:hover:bg-slate-50 dark:border-slate-900 hover:dark:border-green-500 dark:disabled:hover:border-slate-900 dark:disabled:hover:bg-slate-900",
-  text: "border-0 px-3 disabled:cursor-not-allowed  disabled:text-slate-400 hover:text-sky-500  disabled:hover:text-slate-400 dark:hover:text-sky-500 dark:disabled:hover:text-slate-400 ",
+    "ring-0 hover:ring-1 ring-slate-50 py-1 px-3 disabled:text-slate-400 hover:ring-green-500 hover:bg-green-500 hover:bg-opacity-20 disabled:hover:ring-slate-50 disabled:hover:bg-slate-50 dark:ring-slate-900 hover:dark:ring-green-500 dark:disabled:hover:ring-slate-900 dark:disabled:hover:bg-slate-900",
+  text: "px-3 disabled:cursor-not-allowed  disabled:text-slate-400 hover:text-sky-500  disabled:hover:text-slate-400 dark:hover:text-sky-500 dark:disabled:hover:text-slate-400 ",
   "primary-icon":
-    "border-1 rounded-full border-slate-50 py-2 px-2 hover:bg-sky-500 hover:bg-opacity-20 dark:border-slate-900 disabled:text-slate-500 disabled:hover:bg-slate-50 dark:disabled:hover:bg-slate-900 disabled:cursor-not-allowed ",
+    "rounded-full py-2 px-2 hover:bg-sky-500 hover:bg-opacity-50 disabled:text-slate-500 disabled:hover:bg-slate-50 dark:disabled:hover:bg-slate-900 disabled:cursor-not-allowed ",
   "secondary-icon":
-    "border-1 rounded-full border-slate-50 py-2 px-2 hover:bg-slate-500 hover:bg-opacity-20 dark:border-slate-900 disabled:text-slate-500 disabled:hover:bg-slate-50 dark:disabled:hover:bg-slate-900 disabled:cursor-not-allowed ",
+    "rounded-full py-2 px-2 hover:bg-slate-500 hover:bg-opacity-50 disabled:text-slate-500 disabled:hover:bg-slate-50 dark:disabled:hover:bg-slate-900 disabled:cursor-not-allowed ",
   "danger-icon":
-    "border-0 rounded-full border-slate-50 py-2 px-2 hover:bg-rose-500 hover:bg-opacity-50 dark:border-slate-900 disabled:text-slate-500 disabled:hover:bg-slate-50 dark:disabled:hover:bg-slate-900 disabled:cursor-not-allowed ",
+    "rounded-full py-2 px-2 hover:bg-rose-500 hover:bg-opacity-50 disabled:text-slate-500 disabled:hover:bg-slate-50 dark:disabled:hover:bg-slate-900 disabled:cursor-not-allowed ",
 };
 
 function getClassName(variant: ButtonVariant, hidden?: boolean) {
-  let className = "text-bold flex items-center gap-2 rounded border outline-none";
+  let className = "text-bold flex items-center gap-2 rounded outline-none whitespace-nowrap";
   if (hidden) className += " hidden";
   className += " " + variantStyles[variant];
   return className;
