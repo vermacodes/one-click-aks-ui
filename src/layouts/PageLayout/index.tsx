98b6d2e7ceb5196348143386d16baa5e1294b8fc
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-07 22:45:21 -0500

    Refactor SelectedTerraformWorkspaceNotFound component and PageLayout component

--- src/layouts/PageLayout/index.tsx
+++ src/layouts/PageLayout/index.tsx
@@ -7,53 +7,53 @@ import Button from "../../components/UserInterfaceComponents/Button";
 import DarkModeSwitch from "../../components/UserInterfaceComponents/DarkModeSwitch";
 
 type Props = {
-  heading?: string;
-  children: React.ReactNode;
+	heading?: string;
+	children: React.ReactNode;
 };
 
 export default function PageLayout({ heading, children }: Props) {
-  const { darkMode, setDarkMode, navbarOpen, setNavbarOpen } = useGlobalStateContext();
-  const outerDivRef = useRef<null | HTMLDivElement>(null);
+	const { darkMode, setDarkMode, navbarOpen, setNavbarOpen } = useGlobalStateContext();
+	const outerDivRef = useRef<null | HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (outerDivRef.current !== null) {
-      outerDivRef.current.scrollIntoView();
-    }
-  }, [heading]);
+	useEffect(() => {
+		if (outerDivRef.current !== null) {
+			outerDivRef.current.scrollIntoView();
+		}
+	}, [heading]);
 
-  return (
-    <div ref={outerDivRef}>
-      <Detectors />
-      {heading !== undefined && (
-        <div
-          className={`${
-            heading !== "" ? "mb-4 border-b-2 border-slate-500 py-4 " : "mt-6 "
-          } flex items-center justify-between `}
-        >
-          <div className="flex items-center">
-            <Button
-              variant="secondary-icon"
-              className="mr-2 p-4 text-2xl md:mr-0 md:hidden md:p-0"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <FaBars className="text-slate-500" />
-            </Button>
-            <h1 className="text-xl md:text-4xl">{heading}</h1>
-          </div>
-          <div className="flex gap-4">
-            <ResetLabState buttonVariant="text" newLab={true}>
-              <FaPlus /> New Lab
-            </ResetLabState>
-            <DarkModeSwitch
-              handleOnChange={() => setDarkMode(!darkMode)}
-              label=""
-              id="darkModeSwitch"
-              checked={darkMode}
-            />
-          </div>
-        </div>
-      )}
-      {children}
-    </div>
-  );
+	return (
+		<div ref={outerDivRef}>
+			<Detectors />
+			{heading !== undefined && (
+				<div
+					className={`${
+						heading !== "" ? "mb-4 border-b-2 border-slate-500 py-4 " : "mt-6 "
+					} flex items-center justify-between `}
+				>
+					<div className="flex items-center">
+						<Button
+							variant="secondary-icon"
+							className="mr-2 p-4 text-2xl md:mr-0 md:hidden md:p-0"
+							onClick={() => setNavbarOpen(!navbarOpen)}
+						>
+							<FaBars className="text-slate-500" />
+						</Button>
+						<h1 className="text-xl md:text-4xl">{heading}</h1>
+					</div>
+					<div className="flex gap-4">
+						<ResetLabState buttonVariant="text" newLab={true}>
+							<FaPlus /> New Lab
+						</ResetLabState>
+						<DarkModeSwitch
+							handleOnChange={() => setDarkMode(!darkMode)}
+							label=""
+							id="darkModeSwitch"
+							checked={darkMode}
+						/>
+					</div>
+				</div>
+			)}
+			{children}
+		</div>
+	);
 }
