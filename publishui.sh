396016f4db4221a49aceba3ccfd61b1f41d37c65
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-07 21:38:00 -0500

    Fix indentation in publishui.sh

--- publishui.sh
+++ publishui.sh
@@ -10,14 +10,14 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 if [[ "$1" == "" ]]; then
-    sa="actlabs"
+  sa="actlabs"
 else
-    sa=$1
+  sa=$1
 fi
 
 npm run build && cd dist && az storage blob upload-batch -d '$web' --account-name ${sa} -s "." --overwrite --subscription ACT-CSS-Readiness
 
 # if published to default storage account. Purge the endpoint.
 if [[ "$sa" == "actlabs" ]]; then
-    az cdn endpoint purge --subscription ACT-CSS-Readiness -g actlabs-app -n actlabs --profile-name actlabs --content-paths "/*"
+  az cdn endpoint purge --subscription ACT-CSS-Readiness -g actlabs-app -n actlabs --profile-name actlabs --content-paths "/*"
 fi
