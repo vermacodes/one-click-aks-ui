d2e0bae5b3ebf8689c1d2369c7ad56e0a2b3ec72
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-08 15:55:25 -0500

    Add use-debounce package

--- src/pages/LabsGridPage/index.tsx
+++ src/pages/LabsGridPage/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import { useDebounce } from "use-debounce";
 import SelectedDeployment from "../../components/Deployments/SelectedDeployment";
 import LabCard from "../../components/Lab/LabCard";
 import Terminal from "../../components/Terminal";
@@ -10,85 +11,101 @@ import LabGridLayout from "../../layouts/LabGridLayout";
 import PageLayout from "../../layouts/PageLayout";
 
 export default function LabsGridPage() {
-  const { type } = useParams<{ type: LabType }>();
-  const [searchTerm, setSearchTerm] = useState("");
-  const [pageHeading, setPageHeading] = useState("Labs");
-  const { getLabsByType } = useGetLabs();
+	const { type } = useParams<{ type: LabType }>();
+	const location = useLocation();
+	const navigate = useNavigate();
+	const urlParams = new URLSearchParams(location.search);
+	const [filterTerm, setFilterTerm] = useState(urlParams.get("filter") || "");
+	const [pageHeading, setPageHeading] = useState("Labs");
+	const { getLabsByType } = useGetLabs();
 
-  if (!type) {
-    return null;
-  }
+	if (!type) {
+		return null;
+	}
 
-  const { labs, isLoading } = getLabsByType({ labType: type });
+	const { labs, isLoading } = getLabsByType({ labType: type });
 
-  useEffect(() => {
-    if (type.endsWith("lab")) {
-      setPageHeading(type.charAt(0).toUpperCase() + type.slice(1).replace(/lab$/, " Labs"));
-    } else if (type.endsWith("case")) {
-      setPageHeading(type.charAt(0).toUpperCase() + type.slice(1).replace(/case$/, " Cases"));
-    } else if (type.endsWith("assignment")) {
-      setPageHeading("Assigned Readiness Labs");
-    } else if (type.endsWith("challenge")) {
-      setPageHeading("Challenges");
-    } else {
-      setPageHeading("My Saved Labs (Deprecated) - Use Private Labs");
-    }
+	const [debouncedFilterTerm] = useDebounce(filterTerm, 500); // Debounce for 500ms
 
-    document.title = "ACT Labs | " + pageHeading;
-  }, [type, pageHeading]);
+	useEffect(() => {
+		// Update the URL with the filter term when it changes
+		const params = new URLSearchParams(location.search);
+		if (debouncedFilterTerm) {
+			params.set("filter", debouncedFilterTerm);
+		} else {
+			params.delete("filter");
+		}
+		navigate("." + (params.toString() ? "?" + params.toString() : ""), { replace: true });
+	}, [debouncedFilterTerm]);
 
-  const handleSearchChange = (value: string) => setSearchTerm(value);
+	useEffect(() => {
+		if (type.endsWith("lab")) {
+			setPageHeading(type.charAt(0).toUpperCase() + type.slice(1).replace(/lab$/, " Labs"));
+		} else if (type.endsWith("case")) {
+			setPageHeading(type.charAt(0).toUpperCase() + type.slice(1).replace(/case$/, " Cases"));
+		} else if (type.endsWith("assignment")) {
+			setPageHeading("Assigned Readiness Labs");
+		} else if (type.endsWith("challenge")) {
+			setPageHeading("Challenges");
+		} else {
+			setPageHeading("My Saved Labs (Deprecated) - Use Private Labs");
+		}
 
-  const filteredLabs = labs?.filter((lab) =>
-    Object.values(lab).some((value) => value?.toString().toLowerCase().includes(searchTerm.toLowerCase()))
-  );
+		document.title = "ACT Labs | " + pageHeading;
+	}, [type, pageHeading]);
 
-  if (isLoading)
-    return (
-      <PageLayout heading={pageHeading}>
-        <p className="text-4xl">Loading...</p>
-      </PageLayout>
-    );
+	const handleSearchChange = (value: string) => setFilterTerm(value);
 
-  if (!labs?.length) {
-    return (
-      <PageLayout heading={pageHeading}>
-        <div className="flex flex-col gap-4">
-          <p className="text-4xl">No {pageHeading.toLowerCase()} found!</p>
-          <p>There is nothing to show here, some possible reasons -</p>
-          <ul className="list-inside list-disc">
-            <li>You have not created and saved any labs yet.</li>
-            <li>You have no assignments or challenges.</li>
-            <li>You don't have access to view these labs.</li>
-          </ul>
-          <p>
-            Something isn't right?{" "}
-            <Link to={"/feedback"} className="text-sky-500 underline">
-              Let us know
-            </Link>
-            .
-          </p>
-        </div>
-      </PageLayout>
-    );
-  }
+	const filteredLabs = labs?.filter((lab) =>
+		Object.values(lab).some((value) => value?.toString().toLowerCase().includes(filterTerm.toLowerCase()))
+	);
 
-  return (
-    <PageLayout heading={pageHeading}>
-      <SelectedDeployment sticky={false} />
-      <FilterTextBox
-        value={searchTerm}
-        onChange={(value: string) => handleSearchChange(value)}
-        placeHolderText={`Filter ${pageHeading.toLowerCase()}`}
-      />
-      <LabGridLayout>
-        {filteredLabs
-          ?.sort((a, b) => a.name.localeCompare(b.name))
-          .map((lab) => (
-            <LabCard lab={lab} key={lab.id} />
-          ))}
-      </LabGridLayout>
-      <Terminal />
-    </PageLayout>
-  );
+	if (isLoading)
+		return (
+			<PageLayout heading={pageHeading}>
+				<p className="text-4xl">Loading...</p>
+			</PageLayout>
+		);
+
+	if (!labs?.length) {
+		return (
+			<PageLayout heading={pageHeading}>
+				<div className="flex flex-col gap-4">
+					<p className="text-4xl">No {pageHeading.toLowerCase()} found!</p>
+					<p>There is nothing to show here, some possible reasons -</p>
+					<ul className="list-inside list-disc">
+						<li>You have not created and saved any labs yet.</li>
+						<li>You have no assignments or challenges.</li>
+						<li>You don't have access to view these labs.</li>
+					</ul>
+					<p>
+						Something isn't right?{" "}
+						<Link to={"/feedback"} className="text-sky-500 underline">
+							Let us know
+						</Link>
+						.
+					</p>
+				</div>
+			</PageLayout>
+		);
+	}
+
+	return (
+		<PageLayout heading={pageHeading}>
+			<SelectedDeployment sticky={false} />
+			<FilterTextBox
+				value={filterTerm}
+				onChange={(value: string) => handleSearchChange(value)}
+				placeHolderText={`Filter ${pageHeading.toLowerCase()}`}
+			/>
+			<LabGridLayout>
+				{filteredLabs
+					?.sort((a, b) => a.name.localeCompare(b.name))
+					.map((lab) => (
+						<LabCard lab={lab} key={lab.id} />
+					))}
+			</LabGridLayout>
+			<Terminal />
+		</PageLayout>
+	);
 }
