49ce479b2a9dde702e0e45f406a97c0f75a079c9
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-07 22:45:12 -0500

    Refactor ResetServerCache component

--- src/components/Config/ResetServerCache/index.tsx
+++ src/components/Config/ResetServerCache/index.tsx
@@ -7,38 +7,35 @@ import Button from "../../UserInterfaceComponents/Button";
 type Props = {};
 
 export default function ResetServerCache({}: Props) {
-  const { mutateAsync: resetServerCacheAsync } = useResetServerCache();
-  const { actionStatus } = useContext(WebSocketContext);
+	const { mutateAsync: resetServerCacheAsync } = useResetServerCache();
+	const { actionStatus } = useContext(WebSocketContext);
 
-  function handleResetServerCache() {
-    const response = toast.promise(resetServerCacheAsync(), {
-      pending: "Resetting Server Cache...",
-      success: "Server Cache Reset.",
-      error: {
-        render(data: any) {
-          return `Failed to reset server cache. ${data.data.response.data.error}`;
-        },
-      },
-    });
+	function handleResetServerCache() {
+		const response = toast.promise(resetServerCacheAsync(), {
+			pending: "Resetting Server Cache...",
+			success: "Server Cache Reset.",
+			error: {
+				render(data: any) {
+					return `Failed to reset server cache. ${data.data.response.data.error}`;
+				},
+			},
+		});
 
-    response.finally(() => {
-      window.location.reload();
-    });
-  }
-  return (
-    <div className="flex w-60 flex-col gap-2 p-2">
-      <Button
-        variant="danger-outline"
-        disabled={actionStatus.inProgress}
-        onClick={handleResetServerCache}
-      >
-        Reset Server Cache
-      </Button>
-      <p className="text-xs">
-        This can help recover from many silly bugs due to bad data in server
-        cache. Make sure to save your lab before you reset cache. It reloads
-        app.
-      </p>
-    </div>
-  );
+		response.finally(() => {
+			sessionStorage.clear(); // clear all session storage
+			window.location.reload();
+		});
+	}
+
+	return (
+		<div className="flex w-60 flex-col gap-2 p-2">
+			<Button variant="danger-outline" disabled={actionStatus.inProgress} onClick={handleResetServerCache}>
+				Reset Server Cache
+			</Button>
+			<p className="text-xs">
+				This can help recover from many silly bugs due to bad data in server cache. Make sure to save your lab before
+				you reset cache. It reloads app.
+			</p>
+		</div>
+	);
 }
