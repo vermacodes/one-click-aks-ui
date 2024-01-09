dcb4aff343331481b8a725ca0c57df9cd02dc374
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-09 14:07:09 -0500

    Refactor Navbar component

--- src/components/Navigation/Navbar/index.tsx
+++ src/components/Navigation/Navbar/index.tsx
@@ -4,12 +4,12 @@ import Pages from "../Pages";
 import Title from "../Title";
 
 export default function Navbar() {
-  const { navbarOpen, setNavbarOpen } = useGlobalStateContext();
-  return (
-    <nav className="flex h-screen w-full min-w-max flex-col  text-slate-900 dark:text-slate-100">
-      <Title navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
-      <Pages />
-      <FixedPages />
-    </nav>
-  );
+	const { navbarOpen, setNavbarOpen } = useGlobalStateContext();
+	return (
+		<nav className="flex h-screen w-full min-w-max flex-col  text-slate-900 dark:text-slate-100">
+			<Title navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
+			<Pages />
+			<FixedPages />
+		</nav>
+	);
 }
