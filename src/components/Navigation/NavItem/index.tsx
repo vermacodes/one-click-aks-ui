1cdc12e0afb98f0c7ade3f99328161340a37de2c
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-09 14:57:47 -0500

    Update NavItem activeClass to include hover effect

--- src/components/Navigation/NavItem/index.tsx
+++ src/components/Navigation/NavItem/index.tsx
@@ -23,14 +23,14 @@ export default function NavItem({
 	const location = useLocation();
 
 	const isActive = location.pathname.includes(to);
-	const activeClass = isActive ? "bg-sky-200 dark:bg-sky-800" : "";
+	const activeClass = isActive ? "bg-sky-200 dark:bg-sky-800" : "hover:bg-slate-200 dark:hover:bg-slate-800";
 
 	return (
 		<li>
 			<Link to={to}>
 				<Tooltip message={toolTipMessage} delay={toolTipDelay} direction={toolTipDirection}>
 					<button
-						className={`flex h-full w-full items-center justify-start gap-2 rounded py-3 px-4 text-left text-base hover:bg-slate-200 dark:hover:bg-slate-800 ${activeClass}`}
+						className={`flex h-full w-full items-center justify-start gap-2 rounded py-3 px-4 text-left text-base ${activeClass}`}
 					>
 						<div className={`ml-${depth * 4}`}>
 							<div className="flex items-center gap-2 text-base">
