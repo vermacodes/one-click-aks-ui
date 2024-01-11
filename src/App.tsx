d20d1926f11381d9e54aefdf99b255cfe67aecd6
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-10 23:33:53 -0500

    Refactor App component layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,35 @@ import ServerNotification from "./components/ServerNotification";
 import MainLayout from "./layouts/MainLayout";
 
 function App() {
-  const { darkMode } = useGlobalStateContext();
-  return (
-    <div className={`${darkMode ? " dark bg-slate-900 text-slate-200" : " bg-slate-50 text-slate-900"}`}>
-      <AuthProvider>
-        <WebSocketContextProvider>
-          <RootErrorBoundary>
-            <MainLayout />
-            <ManagedServerActivityMonitor />
-            <ToastContainer
-              toastClassName={`${
-                darkMode ? "bg-slate-100" : "bg-slate-800"
-              } relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer`}
-              position="top-right"
-              autoClose={2000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme={darkMode ? "light" : "dark"}
-            />
-            <ServerNotification />
-          </RootErrorBoundary>
-        </WebSocketContextProvider>
-      </AuthProvider>
-    </div>
-  );
+	const { darkMode } = useGlobalStateContext();
+	return (
+		<div className={`${darkMode ? " dark bg-slate-900 text-slate-200" : " bg-slate-50 text-slate-900"}`}>
+			<AuthProvider>
+				<WebSocketContextProvider>
+					<RootErrorBoundary>
+						<MainLayout />
+						<ManagedServerActivityMonitor />
+						<ToastContainer
+							toastClassName={`${
+								darkMode ? "bg-slate-100" : "bg-slate-800"
+							} relative flex p-1 min-h-15 rounded-md justify-between overflow-hidden cursor-pointer`}
+							position="top-right"
+							autoClose={2000}
+							hideProgressBar={false}
+							newestOnTop={false}
+							closeOnClick
+							rtl={false}
+							pauseOnFocusLoss
+							draggable
+							pauseOnHover
+							theme={darkMode ? "light" : "dark"}
+						/>
+						<ServerNotification />
+					</RootErrorBoundary>
+				</WebSocketContextProvider>
+			</AuthProvider>
+		</div>
+	);
 }
 
 export default App;
