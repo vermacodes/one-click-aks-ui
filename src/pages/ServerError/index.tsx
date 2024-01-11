893f1d78de80b6c9ab6d8dce283245f4b133fa05
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-10 23:04:33 -0500

    Update ServerError page layout and title

--- src/pages/ServerError/index.tsx
+++ src/pages/ServerError/index.tsx
@@ -1,26 +1,27 @@
 import { useEffect } from "react";
+import ServerConfig from "../../components/Config/Server/ServerConfig";
 import PageLayout from "../../layouts/PageLayout";
 
 export default function ServerError() {
-  useEffect(() => {
-    document.title = "ACT Labs | Server Error";
-  }, []);
+	useEffect(() => {
+		document.title = "ACT Labs | Where is the server?";
+	}, []);
 
-  return (
-    <PageLayout heading="Server Error">
-      <div className="flex flex-col gap-8 pb-12">
-        <p className="text-3xl text-rose-500">Error: Server Not Reachable 😟</p>
-        <p className="text-2xl text-slate-500">
-          This part of ACT Labs require server to be running.{" "}
-          <a
-            href="https://dev.azure.com/Supportability/AzureContainers/_wiki/wikis/Containers%20Wiki/1280601/Getting-Started"
-            target={"_blank"}
-            className="text-sky-500 underline"
-          >
-            Learn more
-          </a>
-        </p>
-      </div>
-    </PageLayout>
-  );
+	return (
+		<PageLayout heading="Where is the server?">
+			<div className="flex flex-col gap-8 pb-12">
+				<p className="text-xl text-slate-700 dark:text-slate-300">
+					This part of ACT Labs require <span className="font-bold italic underline">your</span> server to be running.{" "}
+					<a
+						href="https://dev.azure.com/Supportability/AzureContainers/_wiki/wikis/Containers%20Wiki/1280601/Getting-Started"
+						target={"_blank"}
+						className="text-sky-500 underline"
+					>
+						Learn more &rarr;
+					</a>
+				</p>
+				<ServerConfig />
+			</div>
+		</PageLayout>
+	);
 }
