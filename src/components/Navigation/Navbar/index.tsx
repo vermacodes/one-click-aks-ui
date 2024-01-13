08b864c613f28e46567faf328e55fed81ff96d60
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-12 21:50:00 -0500

    Refactor Navbar and Pages components

--- src/components/Navigation/Navbar/index.tsx
+++ src/components/Navigation/Navbar/index.tsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
-import { useGlobalStateContext } from "../../Context/GlobalStateContext";
 import FixedPages from "../FixedPages";
 import Pages from "../Pages";
 import Title from "../Title";
 
 export default function Navbar() {
-	const { navbarOpen, setNavbarOpen } = useGlobalStateContext();
-	const [isScrolled, setIsScrolled] = useState(false);
 	return (
 		<nav className="flex h-screen w-full min-w-max flex-col  text-slate-900 dark:text-slate-100">
-			<Title navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} isScrolled={isScrolled} />
-			<Pages setIsScrolled={setIsScrolled} />
+			<Title />
+			<Pages />
 			<FixedPages />
 		</nav>
 	);
--- src/components/Navigation/Title/index.tsx
+++ src/components/Navigation/Title/index.tsx
@@ -1,20 +1,12 @@
 import { FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useGlobalStateContext } from "../../Context/GlobalStateContext";
 import Button from "../../UserInterfaceComponents/Button";
 
-type Props = {
-	navbarOpen: boolean;
-	setNavbarOpen: (open: boolean) => void;
-	isScrolled: boolean;
-};
-
-export default function Title({ navbarOpen, setNavbarOpen, isScrolled }: Props) {
+export default function Title() {
+	const { setNavbarOpen } = useGlobalStateContext();
 	return (
-		<div
-			className={`flex items-center justify-between pb-2 pt-6 transition duration-1000  ${
-				isScrolled ? "shadow-lg shadow-slate-300 duration-300 dark:shadow-slate-700" : ""
-			}`}
-		>
+		<div className={`flex items-center justify-between pb-2 pt-6 transition duration-1000`}>
 			<Link to={"/"}>
 				<h1 className="flex flex-row items-center pl-8 text-2xl font-bold hover:text-sky-500">
 					<img src="/actlabs_logo_rocket.svg" className="mr-2 h-8 w-8"></img>
