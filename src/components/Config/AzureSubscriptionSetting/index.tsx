bbe9ad46495fac668b891681739a5a246de59ba3
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-12 23:07:41 -0500

    Refactor AzureSubscriptionSetting component

--- src/components/Config/AzureSubscriptionSetting/index.tsx
+++ src/components/Config/AzureSubscriptionSetting/index.tsx
@@ -1,33 +1,21 @@
-import { useEffect, useState } from "react";
-import { useAccount } from "../../../hooks/useAccount";
+import { useDefaultAccount } from "../../../hooks/useDefaultAccount";
 import Container from "../../UserInterfaceComponents/Container";
 
 export default function AzureSubscriptionSetting() {
-  const [defaultAccount, setDefaultAccount] = useState<string>("Loading...");
-  const { data: accounts } = useAccount();
+	const { defaultAccount } = useDefaultAccount();
 
-  useEffect(() => {
-    if (accounts) {
-      accounts.forEach((account) => {
-        if (account.isDefault) {
-          setDefaultAccount(account.name);
-        }
-      });
-    }
-  }, [accounts]);
-
-  return (
-    <Container title="Azure Subscription" collapsible={true}>
-      <div className="flex w-full flex-col gap-2 gap-x-2 py-2">
-        {/* <AzureSubscription /> */}
-        <div className="w-full rounded border border-slate-500 p-2">
-          {defaultAccount}
-        </div>
-        <p className="text-xs">
-          To change the subscription, re-deploy the server in the new
-          subscription and make sure the endpoint is updated.
-        </p>
-      </div>
-    </Container>
-  );
+	return (
+		<Container title="Azure Subscription" collapsible={true}>
+			<div className="flex w-full flex-col gap-2 gap-x-2 py-2">
+				{/* <AzureSubscription /> */}
+				<div className="w-full rounded border border-slate-500 p-2">
+					<pre className="whitespace-pre-wrap break-words text-sm">{JSON.stringify(defaultAccount, null, 4)}</pre>
+				</div>
+				<p className="text-xs">
+					To change the subscription, re-deploy the server in the new subscription and make sure the endpoint is
+					updated.
+				</p>
+			</div>
+		</Container>
+	);
 }
