a5099a7513576b7840db615da5ac3c4f1dd68cd5
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-16 10:44:15 -0500

    Refactor AuthContext component

--- src/components/Authentication/AuthenticatingFullScreen/index.tsx
+++ src/components/Authentication/AuthenticatingFullScreen/index.tsx
@@ -5,114 +5,112 @@ import { graphAPIScope } from "../../../authConfig";
 import { GraphData } from "../../../dataStructures";
 
 type TokenRequest = {
-  scopes: string[];
-  account: msal.AccountInfo;
+	scopes: string[];
+	account: msal.AccountInfo;
 };
 
 type Props = {
-  graphResponse: GraphData | undefined;
-  setGraphResponse: (updatedGraphData: GraphData | undefined) => void;
-  profilePhoto: string | undefined;
-  setProfilePhoto: (profilePhoto: string | undefined) => void;
-  graphAPIAccessToken: string | undefined;
-  setGraphAPIAccessToken: (graphAPIAccessToken: string | undefined) => void;
+	setGraphResponse: (updatedGraphData: GraphData | undefined) => void;
+	setProfilePhoto: (profilePhoto: string | undefined) => void;
+	graphAPIAccessToken: string | undefined;
+	setGraphAPIAccessToken: (graphAPIAccessToken: string | undefined) => void;
 };
 
 export default function AuthenticatingFullScreen({
-  setGraphResponse,
-  setProfilePhoto,
-  graphAPIAccessToken,
-  setGraphAPIAccessToken,
+	setGraphResponse,
+	setProfilePhoto,
+	graphAPIAccessToken,
+	setGraphAPIAccessToken,
 }: Props) {
-  const { instance } = useMsal();
-  //const [graphAPIAccessToken, setGraphAPIAccessToken] = useState<string>("");
-  const [graphAPITokenAcquired, setGraphAPITokenAcquired] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
+	const { instance } = useMsal();
+	//const [graphAPIAccessToken, setGraphAPIAccessToken] = useState<string>("");
+	const [graphAPITokenAcquired, setGraphAPITokenAcquired] = useState<boolean>(false);
+	const [loading, setLoading] = useState<boolean>(true);
 
-  // request access tokens after the component has mounted
-  useEffect(() => {
-    RequestGraphAPIAccessToken();
-  }, []);
+	// request access tokens after the component has mounted
+	useEffect(() => {
+		RequestGraphAPIAccessToken();
+	}, []);
 
-  useEffect(() => {
-    if (graphAPITokenAcquired && graphAPIAccessToken !== "") {
-      getGraphData();
-      getProfilePhoto();
-    }
-  }, [graphAPITokenAcquired, graphAPIAccessToken]);
+	useEffect(() => {
+		if (graphAPITokenAcquired && graphAPIAccessToken !== "") {
+			getGraphData();
+			getProfilePhoto();
+		}
+	}, [graphAPITokenAcquired, graphAPIAccessToken]);
 
-  async function getGraphData() {
-    fetch("https://graph.microsoft.com/v1.0/me", {
-      headers: {
-        Authorization: `Bearer ${graphAPIAccessToken}`,
-      },
-    }).then((response) => {
-      if (response.ok) {
-        response.json().then((data) => {
-          setGraphResponse(data);
-          setLoading(false);
-        });
-      }
-    });
-  }
+	async function getGraphData() {
+		fetch("https://graph.microsoft.com/v1.0/me", {
+			headers: {
+				Authorization: `Bearer ${graphAPIAccessToken}`,
+			},
+		}).then((response) => {
+			if (response.ok) {
+				response.json().then((data) => {
+					setGraphResponse(data);
+					setLoading(false);
+				});
+			}
+		});
+	}
 
-  async function getProfilePhoto() {
-    fetch("https://graph.microsoft.com/v1.0/me/photo/$value", {
-      headers: {
-        Authorization: `Bearer ${graphAPIAccessToken}`,
-      },
-    }).then((response) => {
-      if (response.ok) {
-        response.blob().then((data) => {
-          const url = URL.createObjectURL(data);
-          setProfilePhoto(url);
-        });
-      }
-    });
-  }
+	async function getProfilePhoto() {
+		fetch("https://graph.microsoft.com/v1.0/me/photo/$value", {
+			headers: {
+				Authorization: `Bearer ${graphAPIAccessToken}`,
+			},
+		}).then((response) => {
+			if (response.ok) {
+				response.blob().then((data) => {
+					const url = URL.createObjectURL(data);
+					setProfilePhoto(url);
+				});
+			}
+		});
+	}
 
-  async function RequestGraphAPIAccessToken() {
-    await instance.handleRedirectPromise();
+	async function RequestGraphAPIAccessToken() {
+		await instance.handleRedirectPromise();
 
-    const accounts = instance.getAllAccounts();
-    const request = {
-      ...graphAPIScope,
-      account: accounts[0],
-    };
+		const accounts = instance.getAllAccounts();
+		const request = {
+			...graphAPIScope,
+			account: accounts[0],
+		};
 
-    if (accounts.length > 0) {
-      try {
-        await acquireTokenSilently(request);
-      } catch (e) {
-        if (e instanceof msal.InteractionRequiredAuthError) {
-          acquireTokenByRedirect(request);
-        } else {
-          console.error(e);
-        }
-      }
-    } else {
-      acquireTokenByRedirect(request);
-    }
-  }
+		if (accounts.length > 0) {
+			try {
+				await acquireTokenSilently(request);
+			} catch (e) {
+				if (e instanceof msal.InteractionRequiredAuthError) {
+					acquireTokenByRedirect(request);
+				} else {
+					console.error(e);
+				}
+			}
+		} else {
+			acquireTokenByRedirect(request);
+		}
+	}
 
-  async function acquireTokenSilently(request: TokenRequest) {
-    const response = await instance.acquireTokenSilent(request);
-    setGraphAPIAccessToken(response.accessToken);
-    setGraphAPITokenAcquired(true);
-  }
+	async function acquireTokenSilently(request: TokenRequest) {
+		const response = await instance.acquireTokenSilent(request);
+		setGraphAPIAccessToken(response.accessToken);
+		setGraphAPITokenAcquired(true);
+	}
 
-  function acquireTokenByRedirect(request: TokenRequest) {
-    instance.acquireTokenRedirect(request);
-  }
+	function acquireTokenByRedirect(request: TokenRequest) {
+		instance.acquireTokenRedirect(request);
+	}
 
-  if (loading) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <p>Authenticating...</p>
-      </div>
-    );
-  }
+	if (loading) {
+		return (
+			<div className="flex h-screen items-center justify-center">
+				<p>Authenticating...</p>
+			</div>
+		);
+	}
 
-  // Return your main component here
-  return null;
+	// Return your main component here
+	return null;
 }
