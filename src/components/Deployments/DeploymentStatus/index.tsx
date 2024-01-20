a52c749fb58e573bc8983956e4982daaf05ba512
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-19 19:14:08 -0500

    Update package-lock.json and DeploymentStatus component

--- src/components/Deployments/DeploymentStatus/index.tsx
+++ src/components/Deployments/DeploymentStatus/index.tsx
@@ -1,24 +1,36 @@
 import { FaCheckCircle, FaExclamationCircle } from "react-icons/fa";
+import { TbFidgetSpinner } from "react-icons/tb";
 import { DeploymentType } from "../../../dataStructures";
 
 type Props = {
-  deployment: DeploymentType;
+	deployment: DeploymentType;
 };
 
 export default function DeploymentStatus({ deployment }: Props) {
-  return (
-    <div className={`flex items-center gap-2`}>
-      {deployment.deploymentStatus === "Deployment Failed" ||
-      deployment.deploymentStatus === "Destroy Failed" ? (
-        <p className="text-rose-500">
-          <FaExclamationCircle />
-        </p>
-      ) : (
-        <p className="text-green-500">
-          <FaCheckCircle />
-        </p>
-      )}
-      {deployment.deploymentStatus}
-    </div>
-  );
+	if (
+		deployment.deploymentStatus === "Deployment In Progress" ||
+		deployment.deploymentStatus === "Destroy In Progress"
+	) {
+		return (
+			<div className={`flex items-center gap-2`}>
+				<TbFidgetSpinner className="animate-spin text-xl text-sky-500" />
+				{deployment.deploymentStatus}
+			</div>
+		);
+	}
+
+	return (
+		<div className={`flex items-center gap-2`}>
+			{deployment.deploymentStatus === "Deployment Failed" || deployment.deploymentStatus === "Destroy Failed" ? (
+				<p className="text-rose-500">
+					<FaExclamationCircle />
+				</p>
+			) : (
+				<p className="text-green-500">
+					<FaCheckCircle />
+				</p>
+			)}
+			{deployment.deploymentStatus}
+		</div>
+	);
 }
