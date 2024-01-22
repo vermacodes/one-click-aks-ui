3daeac3eba65ca4ac7404c87a74078b6eb3de019
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-22 10:55:07 -0500

    Add Alert component

---
+++ src/components/UserInterfaceComponents/Alert/index.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+type Props = {
+	variant?: "success" | "warning" | "danger" | "info";
+	children: React.ReactNode;
+};
+
+export default function Alert({ variant = "info", children }: Props) {
+	const [color, setColor] = useState<string>("sky");
+
+	useEffect(() => {
+		switch (variant) {
+			case "success":
+				setColor("green");
+				break;
+			case "warning":
+				setColor("amber");
+				break;
+			case "danger":
+				setColor("red");
+				break;
+			case "info":
+				setColor("sky");
+				break;
+		}
+	}, [variant]);
+
+	return (
+		<div className={`top-0 z-20 mt-2 flex rounded border border-${color}-500 bg-${color}-500 bg-opacity-20 p-2`}>
+			{children}
+		</div>
+	);
+}
