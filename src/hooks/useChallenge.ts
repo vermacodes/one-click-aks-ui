a772a487d53928722e15a0e4e65c9d4007833c95
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-31 11:42:30 -0500

    Refactor ChallengeActionButtons component and useGetLabs hook

--- src/hooks/useChallenge.ts
+++ src/hooks/useChallenge.ts
@@ -4,102 +4,106 @@ import { Challenge, Lab } from "../dataStructures";
 import { actlabsHubAxiosInstance } from "../utils/axios-interceptors";
 
 function getChallenges(): Promise<AxiosResponse<Challenge[]>> {
-  return actlabsHubAxiosInstance("challenge");
+	return actlabsHubAxiosInstance("challenge");
 }
 
 export function useGetChallenges() {
-  return useQuery("get-challenges", getChallenges, {
-    select: (data): Challenge[] => {
-      return data.data;
-    },
-    cacheTime: Infinity,
-    staleTime: Infinity,
-  });
+	return useQuery("get-challenges", getChallenges, {
+		select: (data): Challenge[] => {
+			return data.data;
+		},
+		cacheTime: Infinity,
+		staleTime: Infinity,
+	});
 }
 
 function getMyChallenges(): Promise<AxiosResponse<Challenge[]>> {
-  return actlabsHubAxiosInstance("challenge/my");
+	return actlabsHubAxiosInstance("challenge/my");
 }
 
 export function useGetMyChallenges() {
-  return useQuery("get-my-challenges", getMyChallenges, {
-    select: (data): Challenge[] => {
-      return data.data;
-    },
-    cacheTime: Infinity,
-    staleTime: Infinity,
-  });
+	return useQuery("get-my-challenges", getMyChallenges, {
+		select: (data): Challenge[] => {
+			return data.data;
+		},
+		cacheTime: Infinity,
+		staleTime: Infinity,
+	});
 }
 
 function upsertChallenges(challenges: Challenge[]) {
-  return actlabsHubAxiosInstance.post("challenge", challenges);
+	return actlabsHubAxiosInstance.post("challenge", challenges);
 }
 
 export function useUpsertChallenges() {
-  const queryClient = useQueryClient();
-  return useMutation(upsertChallenges, {
-    onSuccess: () => {
-      queryClient.invalidateQueries("get-challenges");
-      queryClient.invalidateQueries("get-my-challenge-labs-redacted");
-      queryClient.invalidateQueries("get-my-challenges");
-    },
-  });
+	const queryClient = useQueryClient();
+	return useMutation(upsertChallenges, {
+		onSuccess: () => {
+			queryClient.invalidateQueries("get-challenges");
+			queryClient.invalidateQueries("get-my-challenge-labs-redacted");
+			queryClient.invalidateQueries("get-all-challenge-labs-redacted");
+			queryClient.invalidateQueries("get-challenges-by-lab-id");
+			queryClient.invalidateQueries("get-my-challenges");
+		},
+	});
 }
 
 function deleteChallenge(challengeId: string) {
-  return actlabsHubAxiosInstance.delete(`challenge/${challengeId}`);
+	return actlabsHubAxiosInstance.delete(`challenge/${challengeId}`);
 }
 
 export function useDeleteChallenge() {
-  const queryClient = useQueryClient();
-  return useMutation(deleteChallenge, {
-    onSuccess: () => {
-      queryClient.invalidateQueries("get-challenges");
-      queryClient.invalidateQueries("get-my-challenge-labs-redacted");
-      queryClient.invalidateQueries("get-my-challenges");
-    },
-  });
+	const queryClient = useQueryClient();
+	return useMutation(deleteChallenge, {
+		onSuccess: () => {
+			queryClient.invalidateQueries("get-challenges");
+			queryClient.invalidateQueries("get-my-challenge-labs-redacted");
+			queryClient.invalidateQueries("get-all-challenge-labs-redacted");
+			queryClient.invalidateQueries("get-challenges-by-lab-id");
+			queryClient.invalidateQueries("get-my-challenges");
+		},
+	});
 }
 
 function getChallengesByLabId(labId: string): Promise<AxiosResponse<Challenge[]>> {
-  return actlabsHubAxiosInstance.get(`challenge/lab/${labId}`);
+	return actlabsHubAxiosInstance.get(`challenge/lab/${labId}`);
 }
 
 export function useGetChallengesByLabId(labId: string) {
-  return useQuery(["get-challenges-by-lab-id", labId], () => getChallengesByLabId(labId), {
-    select: (data): Challenge[] => {
-      return data.data;
-    },
-    cacheTime: Infinity,
-    staleTime: Infinity,
-    enabled: !!labId,
-  });
+	return useQuery(["get-challenges-by-lab-id", labId], () => getChallengesByLabId(labId), {
+		select: (data): Challenge[] => {
+			return data.data;
+		},
+		cacheTime: Infinity,
+		staleTime: Infinity,
+		enabled: !!labId,
+	});
 }
 
 function getAllChallengeLabsRedacted(): Promise<AxiosResponse<Lab[]>> {
-  return actlabsHubAxiosInstance.get("challenge/labs");
+	return actlabsHubAxiosInstance.get("challenge/labs");
 }
 
 export function useGetAllChallengeLabsRedacted() {
-  return useQuery("get-all-challenge-labs-redacted", getAllChallengeLabsRedacted, {
-    select: (data): Lab[] => {
-      return data.data;
-    },
-    cacheTime: Infinity,
-    staleTime: Infinity,
-  });
+	return useQuery("get-all-challenge-labs-redacted", getAllChallengeLabsRedacted, {
+		select: (data): Lab[] => {
+			return data.data;
+		},
+		cacheTime: Infinity,
+		staleTime: Infinity,
+	});
 }
 
 function getMyChallengeLabsRedacted(): Promise<AxiosResponse<Lab[]>> {
-  return actlabsHubAxiosInstance.get(`challenge/labs/my`);
+	return actlabsHubAxiosInstance.get(`challenge/labs/my`);
 }
 
 export function useGetMyChallengeLabsRedacted() {
-  return useQuery("get-my-challenge-labs-redacted", getMyChallengeLabsRedacted, {
-    select: (data): Lab[] => {
-      return data.data;
-    },
-    cacheTime: Infinity,
-    staleTime: Infinity,
-  });
+	return useQuery("get-my-challenge-labs-redacted", getMyChallengeLabsRedacted, {
+		select: (data): Lab[] => {
+			return data.data;
+		},
+		cacheTime: Infinity,
+		staleTime: Infinity,
+	});
 }
--- src/hooks/useGetLabs.ts
+++ src/hooks/useGetLabs.ts
@@ -3,68 +3,68 @@ import { UseQueryResult } from "react-query";
 import { Lab, LabType } from "../dataStructures";
 import { useGetReadinessLabsRedactedByUserId } from "./useAssignment";
 import { useChallengeLabs, usePrivateLabs, useReadinessLabs, useSharedMockCases, useSharedTemplates } from "./useBlobs";
-import { useGetMyChallengeLabsRedacted } from "./useChallenge";
+import { useGetAllChallengeLabsRedacted } from "./useChallenge";
 
 export function useGetLabs() {
-  const [userId, setUserId] = useState<string>("my");
-  type DataSourceType = UseQueryResult<Lab[], unknown>;
+	const [userId, setUserId] = useState<string>("my");
+	type DataSourceType = UseQueryResult<Lab[], unknown>;
 
-  type DataSourcesType = {
-    [key: string]: DataSourceType;
-  };
+	type DataSourcesType = {
+		[key: string]: DataSourceType;
+	};
 
-  const dataSources: DataSourcesType = {
-    privatelab: usePrivateLabs(),
-    publiclab: useSharedTemplates(),
-    mockcase: useSharedMockCases(),
-    // mylabs: useTemplates(), // Deprecated
-    // template: useTemplates(),
-    readinesslab: useReadinessLabs(),
-    challengelab: useChallengeLabs(),
-    assignment: useGetReadinessLabsRedactedByUserId(userId),
-    challenge: useGetMyChallengeLabsRedacted(),
-  };
+	const dataSources: DataSourcesType = {
+		privatelab: usePrivateLabs(),
+		publiclab: useSharedTemplates(),
+		mockcase: useSharedMockCases(),
+		// mylabs: useTemplates(), // Deprecated
+		// template: useTemplates(),
+		readinesslab: useReadinessLabs(),
+		challengelab: useChallengeLabs(),
+		assignment: useGetReadinessLabsRedactedByUserId(userId),
+		challenge: useGetAllChallengeLabsRedacted(),
+	};
 
-  type GetLabsByTypeProps = {
-    labType: LabType;
-    userId?: string;
-  };
-  const getLabsByType = ({ labType, userId = "my" }: GetLabsByTypeProps) => {
-    // this is to tackle the mockcase and mockcases issue
-    if (userId !== "my") {
-      setUserId(userId);
-    }
+	type GetLabsByTypeProps = {
+		labType: LabType;
+		userId?: string;
+	};
+	const getLabsByType = ({ labType, userId = "my" }: GetLabsByTypeProps) => {
+		// this is to tackle the mockcase and mockcases issue
+		if (userId !== "my") {
+			setUserId(userId);
+		}
 
-    const dataSource = dataSources[labType];
-    const { data: labs, isLoading, isFetching } = dataSource;
-    return {
-      labs,
-      isLoading,
-      isFetching,
-    };
-  };
+		const dataSource = dataSources[labType];
+		const { data: labs, isLoading, isFetching } = dataSource;
+		return {
+			labs,
+			isLoading,
+			isFetching,
+		};
+	};
 
-  type GetLabByTypeAndIdProps = {
-    labType?: LabType;
-    labId?: string;
-  };
-  const getLabByTypeAndId = ({ labType, labId }: GetLabByTypeAndIdProps) => {
-    if (!labType || !labId) {
-      return {
-        lab: undefined,
-        isLoading: false,
-        isFetching: false,
-      };
-    }
+	type GetLabByTypeAndIdProps = {
+		labType?: LabType;
+		labId?: string;
+	};
+	const getLabByTypeAndId = ({ labType, labId }: GetLabByTypeAndIdProps) => {
+		if (!labType || !labId) {
+			return {
+				lab: undefined,
+				isLoading: false,
+				isFetching: false,
+			};
+		}
 
-    const { labs, isLoading, isFetching } = getLabsByType({ labType });
-    const lab = labs?.find((lab) => lab.id === labId);
-    return {
-      lab,
-      isLoading,
-      isFetching,
-    };
-  };
+		const { labs, isLoading, isFetching } = getLabsByType({ labType });
+		const lab = labs?.find((lab) => lab.id === labId);
+		return {
+			lab,
+			isLoading,
+			isFetching,
+		};
+	};
 
-  return { getLabsByType, getLabByTypeAndId };
+	return { getLabsByType, getLabByTypeAndId };
 }
