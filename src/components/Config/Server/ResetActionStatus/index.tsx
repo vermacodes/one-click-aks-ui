b6e668c8da8211466ed9a1d7e11df636b4a6bf70
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-02-05 17:06:47 -0500

    Add reset action status button to Docker and ManagedServer components

---
+++ src/components/Config/Server/ResetActionStatus/index.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { ButtonVariant } from "../../../../dataStructures";
+import { axiosInstance } from "../../../../utils/axios-interceptors";
+import Button from "../../../UserInterfaceComponents/Button";
+import ConfirmationModal from "../../../UserInterfaceComponents/Modal/ConfirmationModal";
+
+type Props = {
+	variant?: ButtonVariant;
+	children: React.ReactNode;
+	tooltipMessage?: string;
+	tooltipDelay?: number;
+	tooltipDirection?: "top" | "bottom" | "left" | "right";
+};
+
+export default function ResetActionStatus({
+	variant = "danger-outline",
+	children,
+	tooltipMessage,
+	tooltipDelay,
+	tooltipDirection,
+}: Props) {
+	const [showModal, setShowModal] = useState<boolean>(false);
+
+	function onClickHandler() {
+		const response = toast.promise(axiosInstance.put("actionstatus", { inProgress: false }), {
+			pending: "Resetting action status...",
+			success: {
+				render(data: any) {
+					return `Action status reset completed.`;
+				},
+				autoClose: 2000,
+			},
+			error: {
+				render(data: any) {
+					return `Action status reset failed: ${data.data.response.data.error}`;
+				},
+				autoClose: 5000,
+			},
+		});
+		response.then(() => {
+			axiosInstance.put("logs/endstream");
+		});
+	}
+	return (
+		<>
+			<Button
+				variant={variant}
+				onClick={() => setShowModal(true)}
+				tooltipMessage={tooltipMessage}
+				tooltipDelay={tooltipDelay}
+				tooltipDirection={tooltipDirection}
+			>
+				{children}
+			</Button>
+			{showModal && (
+				<ConfirmationModal
+					title="Confirm Stop Runaway Action"
+					onClose={() => setShowModal(false)}
+					onConfirm={onClickHandler}
+				>
+					<p className="text-lg">Are you sure you want to stop the running action?</p>
+					<p className="text-xs">
+						Reset the action status if you think server is gone crazy. This can breaks the safety net which prevents
+						running duplicate actions on server. This also stops the log stream.
+					</p>
+				</ConfirmationModal>
+			)}
+		</>
+	);
+}
