62cfd82ae12dde2302309f44e6dc0024d1d97fb2
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-02-09 16:27:22 -0500

    Add version property to ManagedServer and disable Auto Destroy for V2 servers

--- src/components/Detectors/Detectors/index.tsx
+++ src/components/Detectors/Detectors/index.tsx
@@ -1,19 +1,18 @@
 import NoSubscriptionsFound from "../NoSubscriptionsFound";
 import SelectedTerraformWorkspaceNotFound from "../SelectedTerraformWorkspaceNotFound";
 import ServerNotConnected from "../ServerNotConnected";
-import StorageAccountNotConfigured from "../StorageAccountNotConfigured";
 import VersionCheck from "../VersionCheck";
 import WebSocketConnectionStatus from "../WebSocketConnectionStatus";
 
 export default function Detectors() {
-  return (
-    <>
-      <ServerNotConnected />
-      <VersionCheck />
-      <WebSocketConnectionStatus />
-      <NoSubscriptionsFound />
-      <StorageAccountNotConfigured />
-      <SelectedTerraformWorkspaceNotFound />
-    </>
-  );
+	return (
+		<>
+			<ServerNotConnected />
+			<VersionCheck />
+			<WebSocketConnectionStatus />
+			<NoSubscriptionsFound />
+			{/* <StorageAccountNotConfigured /> */}
+			<SelectedTerraformWorkspaceNotFound />
+		</>
+	);
 }
