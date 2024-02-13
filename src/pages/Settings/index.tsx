0df8bad27b4b39306816155658a080d0dab6ce41
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-02-12 23:18:21 -0500

    Update button variants and add new components

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -2,12 +2,9 @@ import { useEffect } from "react";
 import ActlabsHubEndpoint from "../../components/Config/ActlabsHubEndpoint";
 import AzureRegion from "../../components/Config/AzureRegion";
 import AzureSubscriptionSetting from "../../components/Config/AzureSubscriptionSetting";
-import ResetActionStatus from "../../components/Config/ResetActionStatus";
-import ResetServerCache from "../../components/Config/ResetServerCache";
 import ServerConfig from "../../components/Config/Server/ServerConfig";
 import TerraformInit from "../../components/Config/TerraformInit";
 import TerraformWorkspaces from "../../components/Config/TerraformWorkspaces";
-import Container from "../../components/UserInterfaceComponents/Container";
 import PageLayout from "../../layouts/PageLayout";
 
 export default function Settings() {
@@ -30,12 +27,12 @@ export default function Settings() {
 
 				<TerraformInit />
 				<TerraformWorkspaces />
-				<Container collapsible={true}>
+				{/* <Container collapsible={true}>
 					<div className="flex flex-col md:flex-row">
 						<ResetActionStatus />
 						<ResetServerCache />
 					</div>
-				</Container>
+				</Container> */}
 			</div>
 		</PageLayout>
 	);
