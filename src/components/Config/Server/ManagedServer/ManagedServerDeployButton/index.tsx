40713f91060f8ba430d7c2a3d1f48951bb584f3c
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-02-15 12:17:25 -0500

    Update code formatting and fix bug in file processing

---
+++ src/components/Config/Server/ManagedServer/ManagedServerDeployButton/index.tsx
@@ -0,0 +1,46 @@
+import { FaRocket } from "react-icons/fa";
+import { toast } from "react-toastify";
+import { ManagedServer } from "../../../../../dataStructures";
+import { useDeployManagedServer } from "../../../../../hooks/useDeployManagedServer";
+import { useManagedServer } from "../../../../../hooks/useManagedServer";
+import { useAuth } from "../../../../Context/AuthContext";
+import Button from "../../../../UserInterfaceComponents/Button";
+
+type Props = {};
+
+export default function ManagedServerDeployButton({}: Props) {
+	const { graphResponse } = useAuth();
+	const { data: managedServer } = useManagedServer();
+	const { lock, handleDeploy } = useDeployManagedServer();
+
+	function onDeployClick() {
+		if (graphResponse === undefined) {
+			toast.error("Failed to deploy managed server. User not authenticated.");
+			return;
+		}
+		handleDeploy({
+			userPrincipalName: graphResponse?.userPrincipalName,
+			userPrincipalId: graphResponse?.id,
+			logLevel: "-4",
+		} as ManagedServer);
+	}
+
+	if (managedServer === undefined) {
+		return null;
+	}
+
+	return (
+		<Button
+			variant="primary-text"
+			disabled={lock || managedServer.status === "Running"}
+			onClick={onDeployClick}
+			tooltipMessage={
+				managedServer.status === "Running"
+					? "Server is already running. If server was just deployed and status is 'Not Running', Please wait for DNS sync to complete and refresh the page after a few minutes. Re-deploy will not help."
+					: undefined
+			}
+		>
+			<FaRocket /> Deploy
+		</Button>
+	);
+}
---
+++ src/components/Config/Server/ManagedServer/ManagedServerStatus/index.tsx
@@ -0,0 +1,30 @@
+import { FaCheckCircle } from "react-icons/fa";
+import { useManagedServer } from "../../../../../hooks/useManagedServer";
+import Tooltip from "../../../../UserInterfaceComponents/Tooltip";
+
+type Props = {};
+
+export default function ManagedServerStatus({}: Props) {
+	const { data: managedServer } = useManagedServer();
+
+	if (!managedServer) return null;
+
+	return (
+		<Tooltip
+			message={
+				`Server Status as seen by Actlabs Hub. ` +
+				`If this shows running and server is still not connecting that can be due to DNS sync delay. ` +
+				`Try opening https://${managedServer.endpoint}/status in a different tab to ensure DNS resolves correctly and ` +
+				`you get a response back. If thee is no response, give it a few minutes and try again.`
+			}
+			delay={500}
+		>
+			<div className="flex w-36 gap-4 rounded border border-slate-500 px-2 py-1">
+				<div className="flex items-center gap-2">
+					{managedServer.status === "Running" && <FaCheckCircle className="text-green-600" />}
+					{managedServer.status}
+				</div>
+			</div>
+		</Tooltip>
+	);
+}
