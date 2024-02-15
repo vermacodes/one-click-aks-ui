325fb55b5be99028406d66c31ee9d7f88236c302
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-02-15 14:12:16 -0500

    Remove Tooltip component and add ManagedServerDomain component

---
+++ src/components/Config/Server/ManagedServer/ManagedServerDomain/index.tsx
@@ -0,0 +1,20 @@
+import { useManagedServer } from "../../../../../hooks/useManagedServer";
+import Tooltip from "../../../../UserInterfaceComponents/Tooltip";
+
+type Props = {};
+
+export default function ManagedServerDomain({}: Props) {
+	const { data: managedServer } = useManagedServer();
+
+	if (managedServer === undefined) {
+		return null;
+	}
+
+	return (
+		<Tooltip message="Your server's domain. Its accessible on https" delay={500}>
+			<div className="flex gap-4 rounded border border-slate-500 px-2 py-1">
+				<span>{managedServer.endpoint !== "" ? managedServer.endpoint : "Deploy server to see endpoint here.."}</span>
+			</div>
+		</Tooltip>
+	);
+}
