696e9d8566b47a6205bf7411c8916f2689b4faa8
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-02-16 11:33:47 -0500

    Add tooltip for unknown deployment status

--- src/components/Deployments/DeploymentStatus/index.tsx
+++ src/components/Deployments/DeploymentStatus/index.tsx
@@ -1,16 +1,33 @@
 import { FaCheckCircle, FaExclamationCircle } from "react-icons/fa";
+import { FaCircleExclamation } from "react-icons/fa6";
 import { TbFidgetSpinner } from "react-icons/tb";
 import { DeploymentType } from "../../../dataStructures";
+import { useWebSocketContext } from "../../Context/WebSocketContext";
+import Tooltip from "../../UserInterfaceComponents/Tooltip";
 
 type Props = {
 	deployment: DeploymentType;
 };
 
 export default function DeploymentStatus({ deployment }: Props) {
+	const { actionStatus } = useWebSocketContext();
+
 	if (
 		deployment.deploymentStatus === "Deployment In Progress" ||
 		deployment.deploymentStatus === "Destroy In Progress"
 	) {
+		if (!actionStatus.inProgress) {
+			return (
+				<Tooltip
+					message={`Can not accurately determine the deployment status. Try repeating the last action manually or re-deploy your server.`}
+				>
+					<div className={`flex items-center gap-2`}>
+						<FaCircleExclamation className="text-amber-500" />
+						Deployment Status Unknown
+					</div>
+				</Tooltip>
+			);
+		}
 		return (
 			<div className={`flex items-center gap-2`}>
 				<TbFidgetSpinner className="animate-spin text-xl text-sky-500" />
