1be064a6347eefe29bf2a84e04e74bd67c8a319f
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-02-29 23:58:20 -0500

    Add useState and useEffect hooks to DefaultNodepoolMax and DefaultNodepoolMin components

--- src/components/Builder/KubernetesCluster/Nodepools/DefaultNodepool/DefaultNodepoolMax/index.tsx
+++ src/components/Builder/KubernetesCluster/Nodepools/DefaultNodepool/DefaultNodepoolMax/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useSetLogs } from "../../../../../../hooks/useLogs";
 import { useGlobalStateContext } from "../../../../../Context/GlobalStateContext";
 import { WebSocketContext } from "../../../../../Context/WebSocketContext";
@@ -9,34 +9,48 @@ type Props = {
 };
 
 export default function DefaultNodepoolMax({ index }: Props) {
+	const [count, setCount] = useState(1);
+
 	const { lab, setLab } = useGlobalStateContext();
 	const { actionStatus } = useContext(WebSocketContext);
 	const { mutate: setLogs } = useSetLogs();
 
+	useEffect(() => {
+		setCount(lab?.template?.kubernetesClusters[index]?.defaultNodePool?.maxCount || 1);
+	}, [lab, index]);
+
 	// Toggle the auto scaling feature
 	const handleOnChange = (value: string) => {
+		const parsedValue = parseInt(value);
 		const newLab = structuredClone(lab);
 		const cluster = newLab?.template?.kubernetesClusters[index];
-		if (cluster?.defaultNodePool?.enableAutoScaling && newLab !== undefined) {
-			cluster.defaultNodePool.maxCount = parseInt(value);
-			!actionStatus.inProgress && setLogs({ logs: JSON.stringify(newLab?.template, null, 4) });
-			setLab(newLab);
+
+		setCount(parsedValue);
+
+		if (!cluster?.defaultNodePool?.enableAutoScaling || isNaN(parsedValue) || parsedValue === 0) {
+			return;
 		}
+
+		cluster.defaultNodePool.maxCount = parsedValue;
+
+		if (!actionStatus.inProgress) {
+			setLogs({ logs: JSON.stringify(newLab?.template, null, 4) });
+		}
+
+		setLab(newLab);
 	};
 
 	// Determine checked and disabled states
-	const disabled =
-		!lab?.template?.kubernetesClusters[index] ||
-		!lab?.template?.kubernetesClusters[index]?.defaultNodePool?.enableAutoScaling;
+	const disabled = !lab?.template?.kubernetesClusters[index]?.defaultNodePool?.enableAutoScaling;
 
 	return (
 		<div>
 			<NumberInput
 				label="Max Nodes"
-				placeholder="1"
 				type="number"
 				min={1}
 				max={100}
+				value={count}
 				disabled={disabled}
 				onChange={(e) => handleOnChange(e.target.value)}
 			/>
--- src/components/Builder/KubernetesCluster/Nodepools/DefaultNodepool/DefaultNodepoolMin/index.tsx
+++ src/components/Builder/KubernetesCluster/Nodepools/DefaultNodepool/DefaultNodepoolMin/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useSetLogs } from "../../../../../../hooks/useLogs";
 import { useGlobalStateContext } from "../../../../../Context/GlobalStateContext";
 import { WebSocketContext } from "../../../../../Context/WebSocketContext";
@@ -9,33 +9,49 @@ type Props = {
 };
 
 export default function DefaultNodepoolMin({ index }: Props) {
+	const [count, setCount] = useState(1);
+
 	const { lab, setLab } = useGlobalStateContext();
 	const { actionStatus } = useContext(WebSocketContext);
 	const { mutate: setLogs } = useSetLogs();
 
-	// Toggle the auto scaling feature
+	useEffect(() => {
+		setCount(lab?.template?.kubernetesClusters[index]?.defaultNodePool?.minCount || 1);
+	}, [lab, index]);
+
 	const handleOnChange = (value: string) => {
+		const parsedValue = parseInt(value);
 		const newLab = structuredClone(lab);
 		const cluster = newLab?.template?.kubernetesClusters[index];
-		if (cluster?.defaultNodePool?.enableAutoScaling && newLab !== undefined) {
-			cluster.defaultNodePool.minCount = parseInt(value);
-			!actionStatus.inProgress && setLogs({ logs: JSON.stringify(newLab?.template, null, 4) });
-			setLab(newLab);
+
+		setCount(parsedValue);
+
+		if (!cluster?.defaultNodePool?.enableAutoScaling || isNaN(parsedValue) || parsedValue === 0) {
+			return;
 		}
+
+		cluster.defaultNodePool.minCount = parsedValue;
+
+		if (cluster.defaultNodePool.maxCount < parsedValue) {
+			cluster.defaultNodePool.maxCount = parsedValue;
+		}
+
+		if (!actionStatus.inProgress) {
+			setLogs({ logs: JSON.stringify(newLab?.template, null, 4) });
+		}
+
+		setLab(newLab);
 	};
 
-	// Determine checked and disabled states
-	const disabled =
-		!lab?.template?.kubernetesClusters[index] ||
-		!lab?.template?.kubernetesClusters[index]?.defaultNodePool?.enableAutoScaling;
+	const disabled = !lab?.template?.kubernetesClusters[index]?.defaultNodePool?.enableAutoScaling;
 
 	return (
 		<NumberInput
 			label="Min Nodes"
-			placeholder="1"
 			type="number"
 			min={1}
 			max={100}
+			value={count}
 			disabled={disabled}
 			onChange={(e) => handleOnChange(e.target.value)}
 		/>
