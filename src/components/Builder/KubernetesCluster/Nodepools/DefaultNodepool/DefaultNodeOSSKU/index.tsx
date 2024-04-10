03f1301f1d4cda4fc62cdb837432dd627ee6d8e5
Adam Margherio <amargherio@microsoft.com>
2024-04-10 09:32:08 -0500

    ui structural stuff

--- src/components/Builder/KubernetesCluster/Nodepools/DefaultNodepool/DefaultNodeOSSKU/index.tsx
+++ src/components/Builder/KubernetesCluster/Nodepools/DefaultNodepool/DefaultNodeOSSKU/index.tsx
@@ -1,42 +1,73 @@
-import { NodeOSSKUs } from "../../../../../../dataStructures";
-import { useNodeOSes } from "../../../../../../hooks/useNodeOSes";
+import { useContext, useEffect, useState } from "react";
 import { useSetLogs } from "../../../../../../hooks/useLogs";
 import { useGlobalStateContext } from "../../../../../Context/GlobalStateContext";
 import { useWebSocketContext } from "../../../../../Context/WebSocketContext";
 import DropdownSelect from "../../../../../UserInterfaceComponents/DropdownSelect";
 
 type Props = {
-    index: number;
+	index: number;
 };
 
 export default function DefaultNodeOSSKU({ index }: Props) {
-    const { nodeOSes, isLoading, isFetching } = useNodeOSes();
+    const nodeOSes = ["Ubuntu", "AzureLinux"]
 
     const { lab, setLab } = useGlobalStateContext();
     const { actionStatus } = useWebSocketContext();
     const { mutate: setLogs } = useSetLogs();
 
-    // determine if OS selection should be disabled
-    const disabled = isLoading || isFetching;
-
-    // Determine the current node OS selection
-    const currentNodeOS = lab?.template?.kubernetesClusters[index]?.defaultNodePool?.nodeOSSKU;
+    const currentNodeOSSKU = lab?.template?.kubernetesClusters[index].defaultNodePool.osSku;
 
     /**
      * Handles selection of the node OS SKU to use for the default node pool
      * 
      * @param {NodeOSSKUs} nodeOS - The selected node OS SKU
      */
-    const handleOnSelect = (nodeOS: string[]) => {
+    const handleOnSelect = (nodeOS: string) => {
         const nodeOSKey = Object.keys(nodeOS)[0];
         const newLab = structuredClone(lab);
         if (newLab?.template && newLab.template.kubernetesClusters[index]) {
-            newLab.template.kubernetesClusters[index].defaultNodePool.nodeOSSKU = nodeOSKey;
+            newLab.template.kubernetesClusters[index].defaultNodePool.osSku = nodeOSKey;
             !actionStatus.inProgress &&
                 setLogs({ logs: JSON.stringify(newLab.template, null, 4) });
             setLab(newLab);
         }
     };
+
+    function handleOnNodeOSChange(nodeOS: string) {
+        // clone the lab object so we don't mutate the state directly,
+        // then set the new node OS SKU and update the lab state
+        const newLab = structuredClone(lab);
+        const cluster = newLab?.template?.kubernetesClusters[index];
+
+        if (!cluster?.defaultNodePool) {
+            return;
+        }
+
+        if (cluster.defaultNodePool.osSku === nodeOS) {
+            return
+        }
+
+        cluster.defaultNodePool.osSku = nodeOS;
+
+        if (!actionStatus.inProgress) {
+            setLogs({ logs: JSON.stringify(newLab?.template, null, 4) });
+        }
+
+        setLab(newLab);
+    }
+
+    const renderItem = (osSku: string) => {
+        return (
+            <div
+				className={`${
+					osSku === currentNodeOSSKU && "bg-green-300 hover:text-slate-900 dark:text-slate-900"
+				} w-full cursor-pointer items-center justify-between rounded p-2 hover:bg-sky-500 hover:text-slate-100`}
+			>
+				{osSku}
+			</div>
+        )
+    }
+
 
     /**
      * Function to render an item
@@ -44,47 +75,16 @@ export default function DefaultNodeOSSKU({ index }: Props) {
      * @param {NodeOSSKUs} nodeOSSKU - string array containing OS SKU values
      * @returns JSX.Element - The rendered item
      */
-    const renderItem = (nodeOSSKU: string[]) => {
-        // Determine the class to apply based on whether the current node OS value matches the 
-
-
-
-
-        const key = Object.keys(nodeOSSKU)[0];
-
-        // Determine the classes to apply based on whether the node OS value matches the keu
-        const classes = 
-            key === currentNodeOS
-            ? "bg-green-300 hover:text-slate-900 dark:text-slate-900"
-            : "";
-        
-        return (
-            <div
-                className={`${classes} mt-2 w-full cursor-pointer items-center justify-between gap-2 rounded p-2 hover:bg-sky-500 hover:text-slate-100`}
-            >
-                <div className="flex justify-between">
-                    <div key={key}>{key}</div>
-                </div>
-                <div className="space-x-2 text-xs text-slate-500">
-                    Node OS SKUs :
-                    {nodeOSSKU[key].map((sku) => (
-                        <span key={sku}>{sku}</span>
-                    ))}
-                </div>
-            </div>
-        )
-    };
-
     return (
-        <div className="w-64">
+        <div className="flex gap-2">
+            <label htmlFor="nodeOSSKU">Node OS SKU</label>
             <DropdownSelect
-                disabled={disabled}
-                heading={isLoading || isFetching ? "Please wait..." : currentNodeOS}
+                heading={currentNodeOSSKU == null ? "Ubuntu" : currentNodeOSSKU}
                 items={nodeOSes}
-                onItemClick={handleOnSelect}
+                onItemClick={handleOnNodeOSChange}
                 renderItem={renderItem}
-                width={"full"}
-                height={"h-60"}
+                tooltipMessage="The operating system to use for the default node pool."
+                tooltipDelay={1000}
             />
         </div>
     )
