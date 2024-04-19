255c94b20accb089ae6225971fe5177d7097b8e1
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-04-19 13:35:23 -0400

    Update recent server version

--- src/components/Detectors/VersionCheck/index.tsx
+++ src/components/Detectors/VersionCheck/index.tsx
@@ -3,38 +3,38 @@ import { useServerStatus } from "../../../hooks/useServerStatus";
 import Alert from "../../UserInterfaceComponents/Alert";
 
 export default function VersionCheck() {
-	const { data, isFetching, isError } = useServerStatus();
+  const { data, isFetching, isError } = useServerStatus();
 
-	//check if version in status is greater than a given number. if so, return true else return false
-	function versionCheck(version: string) {
-		//if version is undefined, return false
-		if (version === undefined || version === "") {
-			return false;
-		}
+  //check if version in status is greater than a given number. if so, return true else return false
+  function versionCheck(version: string) {
+    //if version is undefined, return false
+    if (version === undefined || version === "") {
+      return false;
+    }
 
-		const versionNumber = parseInt(version);
-		return versionNumber >= 20240213;
-	}
+    const versionNumber = parseInt(version);
+    return versionNumber >= 20240419;
+  }
 
-	if (data === undefined || isFetching) {
-		return <></>;
-	}
+  if (data === undefined || isFetching) {
+    return <></>;
+  }
 
-	if (isError || data.status !== "OK") {
-		return <></>;
-	}
+  if (isError || data.status !== "OK") {
+    return <></>;
+  }
 
-	if (versionCheck(data.version)) {
-		return <></>;
-	}
+  if (versionCheck(data.version)) {
+    return <></>;
+  }
 
-	return (
-		<Alert variant="warning">
-			<strong>⚠️ New Version Released:</strong> Goto{" "}
-			<Link to="/settings" className="cursor-pointer text-sky-600 underline">
-				Settings
-			</Link>
-			, and follow steps to deploy server again.
-		</Alert>
-	);
+  return (
+    <Alert variant="warning">
+      <strong>⚠️ New Version Released:</strong> Goto{" "}
+      <Link to="/settings" className="cursor-pointer text-sky-600 underline">
+        Settings
+      </Link>
+      , and follow steps to deploy server again.
+    </Alert>
+  );
 }
