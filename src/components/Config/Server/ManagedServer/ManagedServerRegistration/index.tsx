991515906e513e0d3fe6f1cf46614937fa592c89
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-05-17 10:44:43 -0400

    Remove notice about new tenant

--- src/components/Config/Server/ManagedServer/ManagedServerRegistration/index.tsx
+++ src/components/Config/Server/ManagedServer/ManagedServerRegistration/index.tsx
@@ -12,7 +12,7 @@ import GradientBorderContainer from "../../../../UserInterfaceComponents/Gradien
 
 type Props = {};
 
-export default function ManagedServerRegistration({}: Props) {
+export default function ManagedServerRegistration({ }: Props) {
   const [subscriptionId, setSubscriptionId] = useState<string>("");
   const { mutateAsync: registerManagedServer } = useRegisterSubscription();
   const handleRegistration = () => {
@@ -48,32 +48,41 @@ export default function ManagedServerRegistration({}: Props) {
         <div className="flex flex-col gap-2">
           {/* <ManagedServerRegistrationCommand /> */}
           <p>
-            To use managed server, one time registration is required. Follow the steps below to complete registration.
+            To use managed server, one time registration is required. Follow the
+            steps below to complete registration.
           </p>
           <ol className="ml-6 list-decimal">
             <li className="py-1">
               Open Azure Cloud Shell.
-              <Button variant="primary" onClick={() => window.open("https://shell.azure.com", "_blank")}>
+              <Button
+                variant="primary"
+                onClick={() => window.open("https://shell.azure.com", "_blank")}
+              >
                 <SiMicrosoftazure className="h-full" /> {"Launch Cloud Shell"}
               </Button>
               <p className="mt-2 text-sm text-slate-500">
-                You can also run these steps on your system if you use linux (WSL) and have latest version of Azure CLI
-                installed. We recommend using Azure Cloud Shell.
+                You can also run these steps on your system if you use linux
+                (WSL) and have latest version of Azure CLI installed. We
+                recommend using Azure Cloud Shell.
               </p>
             </li>
-            <li className="py-1">Ensure Azure Cloud Shell is running in 'Bash' mode</li>
             <li className="py-1">
-              Ensure that you've selected correct default account by running following command.
+              Ensure Azure Cloud Shell is running in 'Bash' mode
+            </li>
+            <li className="py-1">
+              Ensure that you've selected correct default account by running
+              following command.
               <CodeBlock codeString="az account show" copyEnabled={true} />
             </li>
             <li className="py-1">
-              Run following command in Azure Cloud Shell to complete configurations.
+              Run following command in Azure Cloud Shell to complete
+              configurations.
               <CodeBlock
                 codeString="curl -o actlabs.sh -sLO https://aka.ms/ActlabsManagedServer; chmod +x actlabs.sh; ./actlabs.sh; rm actlabs.sh"
                 copyEnabled={true}
               />
             </li>
-            <li className="py-1">
+            {/*<li className="py-1">
               {" "}
               Copy subscription id from script's output, paste it in the input box below and click on Register button.
               If you run into issues{" "}
@@ -81,9 +90,9 @@ export default function ManagedServerRegistration({}: Props) {
                 let us know
               </Link>
               .
-            </li>
+            </li>*/}
           </ol>
-          <div className="mt-8 flex gap-4">
+          {/*<div className="mt-8 flex gap-4">
             <input
               className="w-full rounded border border-slate-500 bg-inherit p-2 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 md:w-96"
               placeholder="Paste subscription id from script output here..."
@@ -93,7 +102,7 @@ export default function ManagedServerRegistration({}: Props) {
             <Button variant="primary" onClick={handleRegistration}>
               <FaCheck /> Register
             </Button>
-          </div>
+          </div>*/}
         </div>
       </Container>
     </GradientBorderContainer>
