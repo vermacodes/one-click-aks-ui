52f76a95085bd534ec9d43e9267112c90082c767
hc-github-team-tf-azure <>
2025-04-15 15:21:28 -0400

    Enhance Tooltip component accessibility by adding focus management for keyboard navigation

--- src/components/UserInterfaceComponents/Tooltip/index.tsx
+++ src/components/UserInterfaceComponents/Tooltip/index.tsx
@@ -1,36 +1,42 @@
 import { useEffect, useState } from "react";
 
 type Props = {
-  message?: string;
-  children: React.ReactNode;
-  delay?: number; // in milliseconds
-  direction?: "top" | "bottom" | "left" | "right";
-  align?: "start" | "center" | "end";
+	message?: string;
+	children: React.ReactNode;
+	delay?: number; // in milliseconds
+	direction?: "top" | "bottom" | "left" | "right";
+	align?: "start" | "center" | "end";
 };
 
 export default function Tooltip({ message, children, delay = 100, direction = "bottom", align = "center" }: Props) {
-  const [visible, setVisible] = useState(false);
-  const [mouseOn, setMouseOn] = useState(false);
+	const [visible, setVisible] = useState(false);
+	const [mouseOn, setMouseOn] = useState(false);
 
-  let timer: NodeJS.Timeout;
+	let timer: NodeJS.Timeout;
 
-  useEffect(() => {
-    if (mouseOn) {
-      timer = setTimeout(() => setVisible(true), delay);
-    }
+	useEffect(() => {
+		if (mouseOn) {
+			timer = setTimeout(() => setVisible(true), delay);
+		}
 
-    return () => {
-      clearTimeout(timer);
-      setVisible(false);
-    };
-  }, [mouseOn]);
+		return () => {
+			clearTimeout(timer);
+			setVisible(false);
+		};
+	}, [mouseOn]);
 
-  return (
-    <div className="relative flex" onMouseEnter={() => setMouseOn(true)} onMouseLeave={() => setMouseOn(false)}>
-      {children}
-      {message && visible && (
-        <div
-          className={`absolute
+	return (
+		<div
+			className="relative flex"
+			onMouseEnter={() => setMouseOn(true)}
+			onMouseLeave={() => setMouseOn(false)}
+			onFocus={() => setMouseOn(true)} // Show tooltip when the child is focused
+			onBlur={() => setMouseOn(false)} // Hide tooltip when the child loses focus
+		>
+			{children}
+			{message && visible && (
+				<div
+					className={`absolute
           z-10 w-48 min-w-fit transform whitespace-pre-wrap rounded bg-slate-800 p-2 text-xs text-slate-100
           transition-all  dark:bg-slate-100 dark:text-slate-900 
           ${direction === "top" ? "bottom-full -translate-y-2" : ""}
@@ -40,10 +46,10 @@ export default function Tooltip({ message, children, delay = 100, direction = "b
           ${align === "center" ? "left-1/2 -translate-x-1/2" : ""}
           ${align === "end" ? "right-0" : ""}
         `}
-        >
-          {message}
-        </div>
-      )}
-    </div>
-  );
+				>
+					{message}
+				</div>
+			)}
+		</div>
+	);
 }
