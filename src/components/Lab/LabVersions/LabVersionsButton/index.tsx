5e6fe408acebd35ccd680e709f79a3bab6b026ce
hc-github-team-tf-azure <>
2025-04-15 21:30:04 -0400

    Refactor LabVersionsButton component by simplifying the structure and removing unnecessary Button wrapper

--- src/components/Lab/LabVersions/LabVersionsButton/index.tsx
+++ src/components/Lab/LabVersions/LabVersionsButton/index.tsx
@@ -1,22 +1,19 @@
-import { ButtonVariant, Lab } from "../../../../dataStructures";
-import Button from "../../../UserInterfaceComponents/Button";
-import { Link } from "react-router-dom";
 import { FaHistory } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { ButtonVariant, Lab } from "../../../../dataStructures";
 
 type Props = {
-  variant?: ButtonVariant;
-  lab: Lab;
+	variant?: ButtonVariant;
+	lab: Lab;
 };
 
-export default function LabVersionsButton({
-  variant = "secondary-text",
-  lab,
-}: Props) {
-  return (
-    <Link to={"/lab/versions/" + lab.type + "/" + lab.id}>
-      <Button variant={variant}>
-        <FaHistory /> Versions
-      </Button>
-    </Link>
-  );
+export default function LabVersionsButton({ variant = "secondary-text", lab }: Props) {
+	return (
+		<Link
+			to={"/lab/versions/" + lab.type + "/" + lab.id}
+			className="text-bold flex items-center gap-2 whitespace-nowrap rounded px-3 py-1 text-gray-700 ring-0 focus-visible:outline-2 disabled:text-slate-400 hover:bg-gray-500 hover:bg-opacity-20 hover:ring-1 hover:ring-gray-500 dark:text-gray-400 dark:disabled:text-slate-500 dark:hover:bg-gray-400 dark:hover:bg-opacity-20 dark:hover:ring-gray-400"
+		>
+			<FaHistory /> Versions
+		</Link>
+	);
 }
