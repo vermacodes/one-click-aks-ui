ee9d22a68a073197dcd0bef7a192ed4411a463f6
hc-github-team-tf-azure <>
2025-04-17 23:44:19 -0400

    Refactor PageLayout component for improved readability and consistency

--- src/layouts/PageLayout/index.tsx
+++ src/layouts/PageLayout/index.tsx
@@ -2,40 +2,46 @@ import { useEffect, useRef } from "react";
 import Detectors from "../../components/Detectors/Detectors";
 
 type Props = {
-	heading?: string;
-	children: React.ReactNode;
+  heading?: string;
+  children: React.ReactNode;
 };
 
 export default function PageLayout({ heading, children }: Props) {
-	const outerDivRef = useRef<null | HTMLDivElement>(null);
-	const pageHeading = useRef<null | HTMLHeadingElement>(null);
+  const outerDivRef = useRef<null | HTMLDivElement>(null);
+  const pageHeading = useRef<null | HTMLHeadingElement>(null);
 
-	useEffect(() => {
-		if (outerDivRef.current !== null) {
-			outerDivRef.current.scrollIntoView();
-		}
-		if (pageHeading.current !== null) {
-			pageHeading.current.focus(); // Focus the page heading
-		}
-	}, [heading]);
+  useEffect(() => {
+    if (outerDivRef.current !== null) {
+      outerDivRef.current.scrollIntoView();
+    }
+    if (pageHeading.current !== null) {
+      pageHeading.current.focus(); // Focus the page heading
+    }
+  }, [heading]);
 
-	return (
-		<div ref={outerDivRef}>
-			<Detectors />
-			{heading !== undefined && (
-				<div
-					className={`${
-						heading !== "" ? "mb-4 mt-[1px] border-b-2 border-slate-500 " : "mt-6 "
-					} flex items-center justify-between text-wrap`}
-				>
-					<div className="flex items-center">
-						<h2 className="py-4 text-xl md:text-4xl" tabIndex={-1} ref={pageHeading}>
-							{heading}
-						</h2>
-					</div>
-				</div>
-			)}
-			{children}
-		</div>
-	);
+  return (
+    <div ref={outerDivRef}>
+      <Detectors />
+      {heading !== undefined && (
+        <div
+          className={`${
+            heading !== ""
+              ? "mb-4 mt-[1px] border-b-2 border-slate-500 "
+              : "mt-6 "
+          } flex items-center justify-between text-wrap`}
+        >
+          <div className="flex items-center">
+            <h2
+              className="pb-4 text-xl md:text-4xl"
+              tabIndex={-1}
+              ref={pageHeading}
+            >
+              {heading}
+            </h2>
+          </div>
+        </div>
+      )}
+      {children}
+    </div>
+  );
 }
