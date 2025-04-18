e1f87eee7da45dcf3149d28aed7b894e649a62a4
hc-github-team-tf-azure <>
2025-04-18 10:02:37 -0400

    Refactor Button styles for improved consistency and accessibility; update Deployments component for better readability

--- src/pages/Deployments/index.tsx
+++ src/pages/Deployments/index.tsx
@@ -15,50 +15,53 @@ import PageLayout from "../../layouts/PageLayout";
 import ServerError from "../ServerError";
 
 export default function Deployments() {
-	const { data: deployments } = useGetMyDeployments();
-	const { data: workspaces } = useTerraformWorkspace();
-	const { data: serverStatus } = useServerStatus();
-	const { selectedDeployment } = useSelectedDeployment();
+  const { data: deployments } = useGetMyDeployments();
+  const { data: workspaces } = useTerraformWorkspace();
+  const { data: serverStatus } = useServerStatus();
+  const { selectedDeployment } = useSelectedDeployment();
 
-	useEffect(() => {
-		document.title = "ACT Labs | Deployments";
-	}, []);
+  useEffect(() => {
+    document.title = "ACT Labs | Deployments";
+  }, []);
 
-	if (serverStatus?.status !== "OK") {
-		return <ServerError />;
-	}
+  if (serverStatus?.status !== "OK") {
+    return <ServerError />;
+  }
 
-	if (deployments === undefined || workspaces === undefined) {
-		return (
-			<PageLayout heading="Deployments">
-				<p className="text-4xl">Loading...</p>
-			</PageLayout>
-		);
-	}
+  if (deployments === undefined || workspaces === undefined) {
+    return (
+      <PageLayout heading="Deployments">
+        <p className="text-4xl">Loading...</p>
+      </PageLayout>
+    );
+  }
 
-	return (
-		<PageLayout heading="Deployments">
-			<div className="flex items-center justify-between">
-				<BackButton />
+  return (
+    <PageLayout heading="Deployments">
+      <div className="flex items-center justify-between">
+        <BackButton />
 
-				<div className={`mb-3 flex justify-end gap-x-2 rounded`}>
-					<Link to="/builder">
-						<Button variant="secondary-outline">
-							<FaTools /> Lab Builder
-						</Button>
-					</Link>
-					<CreateNewDeployment variant="primary">
-						<FaPlus /> Add Deployment
-					</CreateNewDeployment>
-					<Button variant="secondary">Test Button</Button>
-				</div>
-			</div>
-			{deployments &&
-				deployments.length > 0 &&
-				deployments.map((deployment: DeploymentType) => (
-					<Deployment deployment={deployment} selectedDeployment={selectedDeployment} key={deployment.deploymentId} />
-				))}
-			<Terminal />
-		</PageLayout>
-	);
+        <div className={`mb-3 flex justify-end gap-x-2 rounded`}>
+          <Link to="/builder">
+            <Button variant="secondary-outline">
+              <FaTools /> Lab Builder
+            </Button>
+          </Link>
+          <CreateNewDeployment variant="primary">
+            <FaPlus /> Add Deployment
+          </CreateNewDeployment>
+        </div>
+      </div>
+      {deployments &&
+        deployments.length > 0 &&
+        deployments.map((deployment: DeploymentType) => (
+          <Deployment
+            deployment={deployment}
+            selectedDeployment={selectedDeployment}
+            key={deployment.deploymentId}
+          />
+        ))}
+      <Terminal />
+    </PageLayout>
+  );
 }
