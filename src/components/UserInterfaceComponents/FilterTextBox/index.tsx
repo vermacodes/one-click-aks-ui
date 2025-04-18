8fadd5e0a7cc6ceac6842f290c59093ecffa0795
hc-github-team-tf-azure <>
2025-04-18 15:17:01 -0400

    Fix placeholder attribute formatting in FilterTextBox component for consistency

--- src/components/UserInterfaceComponents/FilterTextBox/index.tsx
+++ src/components/UserInterfaceComponents/FilterTextBox/index.tsx
@@ -21,7 +21,7 @@ export default function FilterTextBox({
         placeholder={placeHolderText}
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className={`${customClasses} w-full rounded border bg-slate-50 p-2 pl-10 text-lg text-slate-900 shadow placeholder:text-slate-700 focus:outline-none focus:ring-2 focus:ring-sky-500 hover:border-sky-500 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-100 dark:placeholder:text-slate-300 dark:hover:border-sky-500`}
+        className={`${customClasses} w-full rounded border bg-slate-50 p-2 pl-10 text-lg text-slate-900 shadow placeholder:text-slate-700 focus:outline-none focus:ring-2 focus:ring-sky-700 hover:border-sky-700 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-100 dark:placeholder:text-slate-300 dark:focus:ring-sky-500 dark:hover:border-sky-500`}
       />
       <FaFilter className="absolute left-3 top-1/2 -translate-y-1/2 transform text-gray-700 dark:text-gray-300" />
     </div>
