cf035c312f1af5cf4cda71f060973a20db43cf88
hc-github-team-tf-azure <>
2025-04-23 17:35:48 -0400

    refactor: enhance background opacity for Alert component variants

--- src/components/UserInterfaceComponents/Alert/index.tsx
+++ src/components/UserInterfaceComponents/Alert/index.tsx
@@ -13,22 +13,22 @@ export default function Alert({ variant = "info", children, ...rest }: Props) {
     switch (variant) {
       case "success":
         setColor(
-          "border-green-700 dark:border-green-400 bg-green-700/10 dark:bg-green-400/10",
+          "border-green-700 dark:border-green-400 bg-green-700/20 dark:bg-green-400/20",
         );
         break;
       case "warning":
         setColor(
-          "border-amber-700 dark:border-amber-400 bg-amber-700/10 dark:bg-amber-400/10",
+          "border-amber-700 dark:border-amber-400 bg-amber-700/20 dark:bg-amber-400/20",
         );
         break;
       case "danger":
         setColor(
-          "border-rose-700 dark:border-rose-400 bg-rose-700/10 dark:bg-rose-400/10",
+          "border-rose-700 dark:border-rose-400 bg-rose-700/20 dark:bg-rose-400/20",
         );
         break;
       case "info":
         setColor(
-          "border-sky-700 dark:border-sky-400 bg-sky-700/10 dark:bg-sky-400/10",
+          "border-sky-700 dark:border-sky-400 bg-sky-700/20 dark:bg-sky-400/20",
         );
         break;
     }
@@ -39,7 +39,7 @@ export default function Alert({ variant = "info", children, ...rest }: Props) {
       {...rest} // Spread all additional props onto the div
       className={cn(
         color,
-        "bg-opacity-20 top-0 mb-4 rounded-sm border p-2",
+        "top-0 mb-4 rounded-sm border p-2",
         rest.className, // Include any custom className passed via props
       )}
     >
